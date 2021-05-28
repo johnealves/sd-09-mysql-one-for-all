cb7d66115a8e88beec860d66a85257596e3885ac
antonio arieiro <sfgfffs@gmail.com>
2021-05-28 14:25:52 -0300

    faturamento && perfil artist

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW faturamento_atual AS
+    SELECT 
+        (CAST(MIN(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_minimo`,
+        (CAST(MAX(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_maximo`,
+        ROUND(AVG(p.price), 2) AS `faturamento_medio`,
+        (CAST(SUM(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_total`
+    FROM
+        SpotifyClone.plans AS p
+            INNER JOIN
+        SpotifyClone.users AS u ON u.plan_id = p.plan_id;
---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW perfil_artistas AS
+SELECT a.artist_name AS `artista`,
+    al.title AS `album`,
+    COUNT(f.user_id) AS `seguidores`
+FROM SpotifyClone.artists AS a
+    INNER JOIN SpotifyClone.album AS al ON al.artist_id = a.artist_id
+    INNER JOIN SpotifyClone.followers AS f ON f.artist_id = a.artist_id
+GROUP BY `artista`,
+    `album`
+ORDER BY `seguidores` DESC,
+    `artista`,
+    `album`;
