cb08d742065fdc02d92f5f8918d17213273a3716
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-28 17:49:08 -0400

    Fix de novo o challenge 2

--- desafio2.sql
+++ desafio2.sql
@@ -4,6 +4,6 @@ CREATE VIEW estatisticas_musicais AS
     COUNT(DISTINCT art.artist_id) as 'artistas',
     COUNT(DISTINCT alb.album_id) as 'albuns'
   FROM
-    SpotifyClone.artists art
-    INNER JOIN SpotifyClone.albums alb ON alb.artist_id = art.artist_id
-    INNER JOIN SpotifyClone.songs s ON s.album_id = alb.album_id;
+    SpotifyClone.Artists art
+    INNER JOIN SpotifyClone.Albums alb ON alb.artist_id = art.artist_id
+    INNER JOIN SpotifyClone.Songs s ON s.album_id = alb.album_id;
