c4cdaaec834c35e25a20f04ef09cc08a3d407252
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-28 18:31:22 -0300

    desafio 3 cc

--- desafio1.sql
+++ desafio1.sql
@@ -71,7 +71,7 @@ insert into artistas(artista_id, nome) values (1,'Walter Phoenix'), (2,'Peter St
 insert into albuns(album_id, nome, artista_id) values (1,'Envious', 1), (2,'Exuberant',1),
 (3,'Hallowed Steam', 2), (4, 'Incandescent', 3), (5, 'Temporary Culture',4); 
 
-insert into musicas(musica_id, nome, album_id) values (1,'Soul For Us', 1), (2,'Reflections of Magic',1),
+insert into musicas(musica_id, nome, album_id) values (1,'Soul For Us', 1), (2,'Reflections Of Magic',1),
 (3,'Dance With Her Own',1),(4,'Troubles Of My Inner Fire', 2), (5, 'Time Fireworks', 2), 
 (6, 'Magic Circus',3),(7, 'Honey,So Do I',3),(8, "Sweetie, Let's Go Wild",3),(9, 'She Knows',3),
 (10, 'Fantasy For Me', 4),(11, 'Celebration Of More', 4),(12, 'Rock His Everything', 4),
