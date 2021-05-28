892b2527b410c9b2c9bdff02d4969208b32dec8f
Layo Kaminski <layokaminski@gmail.com>
2021-05-28 17:40:28 -0300

    Requisito 5 Finalizado - Requisito 4 troca do group by para ser usado com id do artista

--- desafio4.sql
+++ desafio4.sql
@@ -5,6 +5,6 @@ CREATE VIEW top_3_artistas AS
         SpotifyClone.artists AS a
             INNER JOIN
         SpotifyClone.following_artists AS fa ON fa.artist_id = a.artist_id
-    GROUP BY `artista`
+    GROUP BY a.artist_id
     ORDER BY `seguidores` DESC , `artista` ASC
     LIMIT 3;
