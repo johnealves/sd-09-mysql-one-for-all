156895a267c501350685ed43e1168c9ba626ce60
Osvaldo Borges <netoo70@gmail.com>
2021-05-28 17:55:04 -0300

    Requisito 04

---
+++ desafio4.sql
@@ -0,0 +1,3 @@
+USE SpotifyClone;
+
+CREATE VIEW top_3_artistas AS SELECT a.artista AS 'artista', COUNT(*) AS 'seguidores' FROM SpotifyClone.seguindo_artistas AS sa INNER JOIN SpotifyClone.artistas AS a ON a.artista_id = sa.artista_id GROUP BY sa.artista_id ORDER BY `seguidores` DESC LIMIT 3;
