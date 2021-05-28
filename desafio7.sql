533b655449d95f212846332cb2302d2bdd3486ce
LucioOSilva <lucioosilva00@gmail.com>
2021-05-28 15:39:21 -0300

    added requirement7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW SpotifyClone.perfil_artistas AS
+    SELECT 
+        ar.artista_nome AS artista,
+        al.album_nome AS album,
+        COUNT(s.artista_id) AS seguidores
+    FROM
+        SpotifyClone.Albums AS al
+            INNER JOIN
+        SpotifyClone.Artistas AS ar ON al.artista_id = ar.artista_id
+            INNER JOIN
+        SpotifyClone.Seguidores AS s ON ar.artista_id = s.artista_id
+    GROUP BY ar.artista_id , al.album_nome
+    ORDER BY seguidores DESC , artista;
