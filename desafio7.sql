310a055908d79eb1100899e647c0dcd1590c227c
BernardoFontes <bfontes.1997@gmail.com>
2021-05-28 18:00:01 -0300

    7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 CREATE VIEW perfil_artistas AS SELECT ar.artista_name AS "artista", 
 al.album_name AS "album",
-COUNT(*) AS "seguidores"
+COUNT(s.artista_id) AS "seguidores"
 FROM SpotifyClone.seguidores AS s
 INNER JOIN SpotifyClone.album AS al ON al.artista_id = s.artista_id
 INNER JOIN SpotifyClone.artistas AS ar ON ar.artista_id = s.artista_id
-GROUP BY artista, album, seguidores
+GROUP BY ar.artista_name, al.album_name, s.artista_id
 ORDER BY seguidores DESC, artista, album;
