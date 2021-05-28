8dfb0e89a030a3441fc1c180a05e64a836ed06d0
Luciano_Lodi <lrlodi@gmail.com>
2021-05-28 19:22:48 -0300

    Corrige requisitos 3 e 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
-CREATE VIEW historico_reproducao_usuario AS
+CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-u.usuario_nome AS usuario,
-c.cancao_nome AS nome
+u.usuario_nome AS `usuario`,
+c.cancao_nome AS `nome`
 FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.usuario_historico AS uh ON uh.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.cancoes AS c ON c.cancao_id = uh.cancao_id
-ORDER BY `usuario`;
+ORDER BY `usuario` ASC, `nome` ASC;
