3ec9da278a8143e53f5b59e25acc594d3d5152a9
BernardoFontes <bfontes.1997@gmail.com>
2021-05-28 17:56:12 -0300

    10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 USE SpotifyClone;
 
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(userid INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantidade INT;
@@ -10,9 +10,8 @@ SELECT
 COUNT(*)
 FROM
 SpotifyClone.historico AS h
-GROUP BY h.user_id
-HAVING h.user_id = user_id INTO quantidade;
-
+WHERE user_id = userid
+INTO quantidade;
 RETURN quantidade;
 
 END
