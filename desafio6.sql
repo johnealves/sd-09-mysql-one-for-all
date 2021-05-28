c8bc26bc9afb610a32077e49b9dbef369630230e
antonio arieiro <sfgfffs@gmail.com>
2021-05-28 14:30:16 -0300

    trigger 8

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW faturamento_atual AS
     SELECT 
         (CAST(MIN(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_minimo`,
-        (CAST(MAX(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento maximo`,
+        (CAST(MAX(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_maximo`,
         ROUND(AVG(p.price), 2) AS `faturamento_medio`,
-        (CAST(SUM(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento total`
+        (CAST(SUM(p.price) AS DECIMAL (5 , 2 ))) AS `faturamento_total`
     FROM
         SpotifyClone.plans AS p
             INNER JOIN
