28dbebcf900ddffd70edf21b582b5e173558e29c
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-28 18:05:53 -0400

    Fix challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -3,7 +3,7 @@ CREATE VIEW historico_reproducao_usuarios AS
     u.user as 'usuario',
     s.song as 'nome'
   FROM
-    spotifyclone.listened_to lt
-    INNER JOIN spotifyclone.users u ON u.user_id = lt.user_id
-    INNER JOIN spotifyclone.songs s ON s.song_id = lt.song_id
+    SpotifyClone.Listened_to lt
+    INNER JOIN SpotifyClone.Users u ON u.user_id = lt.user_id
+    INNER JOIN SpotifyClone.Songs s ON s.song_id = lt.song_id
   ORDER BY `usuario` ASC, `nome` ASC;
