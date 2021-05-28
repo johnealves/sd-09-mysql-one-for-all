306d28e70df4fa7d8f49ca18a436b03f13eb99de
tiago-yoneda <xyonedax@gmail.com>
2021-05-28 16:38:51 -0300

    desafio 1 - completed

---
+++ desafio1.sql
@@ -0,0 +1,120 @@
+-- 1 - Crie um banco com o nome de SpotifyClone.
+-- Providencie as queries necessárias para criar tabelas normalizadas que atendam aos requisitos descritos na seção anterior;
+-- Providencie as queries necessárias para popular as tabelas de acordo com os dados listados na seção anterior;
+-- Crie um arquivo de configurações desafio1.json, que mapeará em qual tabela e coluna se encontram as informações necessárias para a avaliação automatizada deste desafio.
+-- As configurações devem possuir o seguinte formato:
+
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+-- Criando as tabelas
+CREATE TABLE plans (
+    plan_id INT PRIMARY KEY AUTO_INCREMENT,
+    plan_name VARCHAR(15) NOT NULL,
+    plan_price DECIMAL(5,2) NOT NULL
+) Engine = InnoDB;
+
+CREATE TABLE users(
+    user_id  INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(30),
+    user_age INT,
+    plan_id INT,
+    FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+) Engine = InnoDB;
+
+CREATE TABLE artists (
+    artist_id  INT PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(50)
+) Engine = InnoDB;
+
+CREATE TABLE albums (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_name VARCHAR(30),
+    artist_id INT,
+    CONSTRAINT UNIQUE (album_name, artist_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) Engine = InnoDB;
+
+CREATE TABLE songs (
+    song_id INT PRIMARY KEY AUTO_INCREMENT,
+    song_title VARCHAR(35),
+    album_id INT,
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+) Engine = InnoDB;
+
+CREATE TABLE user_history (
+    user_id INT,
+    song_id INT,
+    CONSTRAINT PRIMARY KEY (user_id, song_id),
+    FOREIGN KEY (user_id) REFERENCES `users`(user_id),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id)
+) Engine = InnoDB;
+
+CREATE TABLE user_following (
+    user_id INT,
+    artist_id INT,
+    CONSTRAINT PRIMARY KEY (user_id, artist_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) Engine = InnoDB;
+
+-- Polulando as Tabelas
+
+INSERT INTO plans(plan_name, plan_price) VALUES
+('gratuito', 0),
+('universitário',5.99),
+('familiar', 7.99);
+
+INSERT INTO users(user_name, user_age, plan_id) VALUES
+('Thati', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
+
+INSERT INTO artists(artist_name) VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Fred Shannon');
+
+INSERT INTO albums(album_name, artist_id) VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO songs(song_title, album_id) VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO user_history(user_id, song_id) VALUES
+(1, 1), (1, 6), (1, 14), (1, 16),
+(2, 13), (2, 17), (2, 2), (2, 15),
+(3, 4), (3, 16), (3, 6),
+(4, 3), (4, 18), (4, 11);
+
+INSERT INTO user_following(user_id, artist_id) VALUES
+(1, 1), (1, 4), (1, 3),
+(2, 1), (2, 3),
+(3, 2), (3, 1),
+(4, 4);
