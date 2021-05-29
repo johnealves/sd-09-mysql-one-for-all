c686e09eb61b1215efe0ca2278980ef503fe3cfd
PedroMCHenrique <pedro.876.ph@gmail.com>
2021-05-29 16:37:44 -0300

    requisito 1

---
+++ desafio1.sql
@@ -0,0 +1,132 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE `plano`(
+    `plano_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(50) NOT NULL,
+    `valor_plano` DOUBLE NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE `usuarios`(
+    `usuario_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `usuario` VARCHAR(250) NOT NULL,
+    `idade` INT NOT NULL,
+    `plano_id` INT NOT NULL,
+    FOREIGN KEY (`plano_id`) REFERENCES `plano`(`plano_id`)
+) engine = InnoDB;
+
+CREATE TABLE `artistas` (
+	`artista_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `artista_nome` VARCHAR(250) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE `albums` (
+	`album_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `artista_id` INT NOT NULL,
+    `album` VARCHAR(250) NOT NULL,
+    FOREIGN KEY(`artista_id`) REFERENCES `artistas`(`artista_id`)
+) engine = InnoDB;
+
+CREATE TABLE `cancoes` (
+	`cancao_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `album_id` INT NOT NULL,
+    `cancao` VARCHAR(250) NOT NULL,
+    FOREIGN KEY(`album_id`) REFERENCES `albums`(`album_id`)
+) engine = InnoDB;
+
+CREATE TABLE `historico_reproducao` (
+	`usuario_id` INT NOT NULL,
+    `cancao_id` INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (`usuario_id`, `cancao_id`),
+    FOREIGN KEY(`usuario_id`) REFERENCES `usuarios`(`usuario_id`),
+    FOREIGN KEY(`cancao_id`) REFERENCES `cancoes`(`cancao_id`)
+) engine = InnoDB;
+
+CREATE TABLE `seguindo_artista` (
+	`usuario_id` INT NOT NULL,
+    `artista_id` INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (`usuario_id`, `artista_id`),
+    FOREIGN KEY(`usuario_id`) REFERENCES `usuarios`(`usuario_id`),
+    FOREIGN KEY(`artista_id`) REFERENCES `artistas`(`artista_id`)
+) engine = InnoDB;
+
+INSERT INTO `plano` (`nome`, `valor_plano`)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO `artistas` (`artista_nome`)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+  
+INSERT INTO `usuarios` (`usuario`, `idade`, `plano_id`)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+  
+INSERT INTO `albums` (`artista_id`, `album`)
+VALUES
+  (1, 'Envious'),
+  (1, 'Exuberant'),
+  (2, 'Hallowed Steam'),
+  (3, 'Incandescent'),
+  (4, 'Temporary Culture');
+  
+INSERT INTO `cancoes` (`album_id`, `cancao`)
+VALUES
+  (1, 'Soul For Us'),
+  (1, 'Reflections Of Magic'),
+  (1, 'Dance With Her Own'),
+  (2, 'Troubles Of My Inner Fire'),
+  (2, 'Time Fireworks'),
+  (3, 'Magic Circus'),
+  (3, 'Honey, So Do I'),
+  (3, 'Sweetie, Lets Go Wild'),
+  (3, 'She Knows'),
+  (4, 'Fantasy For Me'),
+  (4, 'Celebration Of More'),
+  (4, 'Rock His Everything'),
+  (4, 'Home Forever'),
+  (4, 'Diamond Power'),
+  (4, 'Honey, Lets Be Silly'),
+  (5, 'Thang Of Thunder'),
+  (5, 'Words Of Her Life'),
+  (5, 'Without My Streets');
+  
+  INSERT INTO `historico_reproducao` (`usuario_id`, `cancao_id`)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 14),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
+  
+  INSERT INTO `seguindo_artista` (`usuario_id`, `artista_id`)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
+  
