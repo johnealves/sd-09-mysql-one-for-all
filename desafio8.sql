cc38170cc58d1a4eb0de76088a0acd985030d636
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-29 13:04:34 -0300

    desafio 8 cc

--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,10 @@ create trigger trigger_usuario_delete
 before delete on SpotifyClone.usuarios
 for each row
 begin
-delete from SpotifyClone.seguidores as se
-where se.usuario_id = old.usuario_id;
-delete from SpotifyClone.historico as hi
-where hi.usuario_id = old.usuario_id;
+delete from SpotifyClone.seguidores
+where usuario_id = old.usuario_id;
+delete from SpotifyClone.historico
+where usuario_id = old.usuario_id;
 end $$
+
+delimiter ;
