a222cea708c24df008db54413dc7f33050823d20
Nathália <nathaliaveneziano@icloud.com>
2021-05-28 22:35:40 -0300

    FIX - Normalização e Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -97,13 +97,13 @@ CREATE TABLE Seguidores(
 INSERT INTO Seguidores (usuario_id, artista_id)
 VALUES
   (1, 1),
-  (1, 3),
+  (1, 2),
   (1, 4),
-  (2, 1),
-  (2, 3),
-  (3, 1),
-  (3, 2),
-  (4, 4);
+  (2, 2),
+  (2, 4),
+  (3, 3),
+  (3, 4),
+  (4, 1);
 
 CREATE TABLE Historico(
     usuario_id INT(11),
