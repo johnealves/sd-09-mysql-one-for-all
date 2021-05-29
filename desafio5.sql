72779ce3c4400fb471d90ef8da423e35768c6d91
Daniela Muller <danielamuller20@gmail.com>
2021-05-29 16:55:15 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        c.cancao AS cancao, COUNT(c.cancao_id) AS reproducoes
+    FROM
+        SpotifyClone.cancoes AS c
+            INNER JOIN
+        SpotifyClone.historico_reproducao AS hr ON c.cancao_id = hr.cancao_id
+    GROUP BY c.cancao
+    ORDER BY reproducoes DESC , cancao ASC
+    LIMIT 2;
