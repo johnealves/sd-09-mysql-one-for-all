0800568b83d28cb1ebac081e59d0d7727d73058c
Osvaldo Borges <netoo70@gmail.com>
2021-05-28 22:06:53 -0300

    Li errado o README, kkkkkkkk

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(45))
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE quantidade INT;
-	SELECT (SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes AS hr GROUP BY hr.usuario_id HAVING hr.usuario_id = u.usuario_id) AS 'quantidade_musicas_no_historico' FROM SpotifyClone.usuarios AS u WHERE u.usuario = nome INTO quantidade;
+	SELECT (SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes AS hr GROUP BY hr.usuario_id HAVING hr.usuario_id = u.usuario_id) AS 'quantidade_musicas_no_historico' FROM SpotifyClone.usuarios AS u WHERE u.usuario_id = id INTO quantidade;
     RETURN quantidade;
 END $$
 
