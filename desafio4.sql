70dac90b298ab66ea60335a974be2f0408603ba0
Pablo Fernandes <pabloprates@gmail.com>
2021-05-29 17:36:41 -0300

    Challenge four

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        ca.artista, COUNT(usa.artista_id) AS `seguidores`
+    FROM
+        artistas AS ca,
+        usuarios_seguindo_artistas AS usa
+    WHERE
+        ca.artista_id = usa.artista_id
+    GROUP BY ca.artista_id
+    ORDER BY `seguidores` DESC, ca.artista ASC
