b8e0d556ae60797c79cf658a5287d7d3d5b3eac8
Daniela Muller <danielamuller20@gmail.com>
2021-05-29 18:59:34 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        CONVERT( TRUNCATE(MIN(p.valor_plano), 2) , CHAR) AS faturamento_minimo,
+        CONVERT( ROUND(MAX(p.valor_plano), 2) , CHAR) AS faturamento_maximo,
+        CONVERT( ROUND(AVG(ROUND(p.valor_plano)), 2) , CHAR) AS faturamento_medio,
+        CONVERT( ROUND(SUM(p.valor_plano), 2) , CHAR) AS faturamento_total
+    FROM
+        SpotifyClone.planos AS p
+            INNER JOIN
+        SpotifyClone.usuarios AS u ON u.plano_id = p.plano_id;
