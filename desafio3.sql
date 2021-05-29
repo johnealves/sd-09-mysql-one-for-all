15ba23898a3d81b634986e80e84c7f78bda87e7b
Pablo Fernandes <pabloprates@gmail.com>
2021-05-29 17:30:34 -0300

    Challenge three

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 create view historico_reproducao_usuarios as
-select us.usuario, ca.cancao
+select us.usuario, ca.cancao as `nome`
 from usuarios as us, cancoes as ca, historico_reproducoes_unicas as hru
 where us.usuario_id = hru.usuario_id and ca.cancao_id = hru.cancao_id
-order by us.usuario, ca.cancao;
+order by us.usuario, `nome`;
