23fd2e4b001964ea9bcafcf8243c8cf114aa7cbb
Pablo Fernandes <pabloprates@gmail.com>
2021-05-29 17:55:48 -0300

    Challenge six

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        FORMAT(MIN(pl.valor_plano), 2) AS `faturamento_minimo`,
+        FORMAT(MAX(pl.valor_plano), 2) AS `faturamento_maximo`,
+        FORMAT(AVG(pl.valor_plano), 2) AS `faturamento_medio`,
+        SUM(pl.valor_plano) AS `faturamento_total`
+    FROM
+        planos AS pl,
+        usuarios AS us
+    WHERE
+        pl.plano_id = us.plano_id;
