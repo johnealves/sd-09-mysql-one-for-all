8094350a86ccad5f5326fcb38cfe29bbb2526a04
cleber-teixeira <cleberlt@gmail.com>
2021-05-29 05:30:19 -0300

    Desafio 6 - Refatoração. :white_check_mark:

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
   SELECT FORMAT(ROUND(MIN(p.plan_value), 2), 2) AS faturamento_minimo,
     FORMAT(ROUND(MAX(p.plan_value), 2), 2) AS faturamento_maximo,
     FORMAT(ROUND(AVG(p.plan_value), 2), 2) AS faturamento_medio,
-    FORMAT(ROUND(SUM(p.plan_value), 2), 2) AS faturamento_totol
+    FORMAT(ROUND(SUM(p.plan_value), 2), 2) AS faturamento_total
   FROM SpotifyClone.users AS u
   INNER JOIN SpotifyClone.plan AS p
   ON u.plan_id = p.plan_id;
