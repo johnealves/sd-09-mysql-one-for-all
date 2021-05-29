a8d7cfeaa24997a6abb4b1b8f78fcfdd3d5947ab
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-29 16:03:44 -0300

    Requisito 1

---
+++ desafio1.sql
@@ -0,0 +1,131 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE SCHEMA IF NOT EXISTS SpotifyClone;
+USE SpotifyClone;
+
+CREATE TABLE plano(
+	plano_id INT PRIMARY KEY AUTO_INCREMENT,
+	plano_tipo VARCHAR(50) NOT NULL,
+    plano_valor DECIMAL(5, 2) NOT NULL
+)ENGINE InnoDB;
+
+INSERT INTO plano (plano_tipo, plano_valor)
+VALUES
+	('gratuito', '0'),
+	('familiar', '7.99'),
+	('universitario', '5.99');
+
+CREATE TABLE usuario(
+	usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_nome VARCHAR(50) NOT NULL,
+    usuario_idade INT NOT NULL,
+    plano_id INT NOT NULL,
+	CONSTRAINT fk_plano_id FOREIGN KEY (plano_id) REFERENCES plano (plano_id)
+)ENGINE InnoDB;
+
+INSERT INTO usuario (usuario_nome, usuario_idade, plano_id)
+VALUES
+	('Thati', '23', '1'),
+	('Cintia', '35', '2'),
+	('Bill', '20', '3'),
+	('Roger', '45', '1');
+
+CREATE TABLE artista(
+	artista_id INT PRIMARY KEY AUTO_INCREMENT,
+	artista_nome VARCHAR(50) NOT NULL
+)ENGINE InnoDB;
+
+INSERT INTO artista (artista_nome)
+VALUES
+    ('Walter Phoenix'),
+    ('Peter Strong'),
+    ('Lance Day'),
+    ('Freedie Shannon');
+
+CREATE TABLE album(
+	album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_nome VARCHAR(50) NOT NULL,
+    artista_id INT NOT NULL,
+    CONSTRAINT fk_album_artista_id FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
+)ENGINE InnoDB;
+
+INSERT INTO album (album_nome, artista_id)
+VALUES
+	('Envious', '1'),
+	('Exuberant', '1'),
+	('Hallowed Steam', '2'),
+	('Incandescent', '3'),
+	('Temporary Culture', '4');
+
+CREATE TABLE cancao(
+	cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+    cancao_nome VARCHAR(50),
+    album_id INT NULL,
+    CONSTRAINT fk_cancao_album_id FOREIGN KEY (album_id) REFERENCES album (album_id)
+)ENGINE InnoDB;
+
+INSERT INTO cancao (cancao_nome, album_id)
+VALUES
+	("Soul For Us", 1),
+    ("Reflections Of Magic", 1),
+    ("Dance With Her Own", 1),
+	("Troubles Of My Inner Fire", 2),
+    ("Time Fireworks", 2),
+	("Magic Circus", 3),
+	("Honey, So Do I", 3),
+	("Sweetie, Let's Go Wild", 3),
+	("She Knows", 3),
+	("Fantasy For Me", 4),
+	("Celebration Of More", 4),
+	("Rock His Everything", 4),
+    ("Home Forever", 4),
+    ("Diamond Power", 4),
+    ("Honey, Let's Be Silly", 4),
+	("Thang Of Thunder", 5),
+    ("Words Of Her Life", 5),
+    ("Without My Streets", 5);
+
+CREATE TABLE seguindo_artistas(
+	artista_id INT NOT NULL,
+	usuario_id INT NOT NULL,
+    PRIMARY KEY (artista_id, usuario_id),
+    CONSTRAINT fk_seguindo_artistas_artista_id FOREIGN KEY (artista_id) REFERENCES artista (artista_id),
+	CONSTRAINT fk_seguindo_artistas_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id)
+)ENGINE InnoDB;
+
+INSERT INTO seguindo_artistas (usuario_id, artista_id)
+VALUES
+	(1, 1),
+    (1, 4),
+	(1, 3),
+	(2, 1),
+    (2, 3),
+    (3, 2),
+    (3, 1),
+    (4, 4);
+
+CREATE TABLE historico_de_reproducoes(
+	usuario_id INT NOT NULL,
+    cancao_id INT NOT NULL,
+    PRIMARY KEY (usuario_id, cancao_id),
+    CONSTRAINT fk_historico_de_reproducoes_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
+	CONSTRAINT fk_historico_de_reproducoes_cancao_id FOREIGN KEY (cancao_id) REFERENCES cancao (cancao_id)
+)ENGINE InnoDB;
+
+INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
+VALUES
+	(1, 1),
+	(1, 6),
+    (1, 14),
+    (1, 16),
+    (2, 13),
+    (2, 17),
+    (2, 2),
+    (2, 15),
+    (3, 4),
+    (3, 16),
+    (3, 6),
+    (4, 3),
+    (4, 18),
+    (4, 11);
+
+    select * from SpotifyClone.historico_de_reproducoes;
