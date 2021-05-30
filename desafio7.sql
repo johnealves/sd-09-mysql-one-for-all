b3c67173116be9f4d8b73874a91e95ad6764ff46
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-05-30 18:16:07 -0300

    requisito 7 feito

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        t1.artist AS artista,
+        t2.album AS album,
+        COUNT(t3.user_id) AS seguidores
+    FROM
+        SpotifyClone.artists AS t1
+            INNER JOIN
+        SpotifyClone.albums AS t2 ON t1.artist_id = t2.artist_id
+            INNER JOIN
+        SpotifyClone.followers AS t3 ON t1.artist_id = t3.artist_id
+    GROUP BY t1.artist_id , t2.album_id
+    ORDER BY 3 DESC , 1 ASC , 2 ASC;
