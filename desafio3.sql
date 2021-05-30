4b915f61e7f59593be7f6f1cef31e47b72055acf
edison-ps <edison@osite.com.br>
2021-05-30 15:19:05 -0300

    Requisitos 5 V1.01

--- desafio3.sql
+++ desafio3.sql
@@ -2,7 +2,8 @@ CREATE VIEW historico_reproducao_usuarios AS
 	SELECT
 		usu.usuario AS usuario,
         can.cancao AS nome
-	FROM SpotifyClone.reproducao AS rep
+	FROM SpotifyClone.Reproducao AS rep
     INNER JOIN SpotifyClone.Usuario AS usu ON usu.usuario_id = rep.usuario_id
     INNER JOIN SpotifyClone.Cancao AS can ON can.cancao_id = rep.cancao_id
     ORDER BY usu.usuario, can.cancao;
+    
