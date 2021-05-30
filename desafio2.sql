1648daca402fff9e001fb72e73f0b1e2d7b2443c
saulokirchmaier <saulokirchmaier@gmail.com>
2021-05-30 15:34:47 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT COUNT(DISTINCT(cancoes.cancao_id)) AS 'cancoes',
+COUNT(DISTINCT(artista.artista_id)) AS 'artistas',
+COUNT(DISTINCT(album.album_id)) AS 'albuns'
+FROM SpotifyClone.cancoes, SpotifyClone.artista, SpotifyClone.album;
