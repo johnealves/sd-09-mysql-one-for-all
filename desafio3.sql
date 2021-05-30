d869b6b3ef16db91b818e7af2487645fa2194ef7
jhon <jhonatan.arguello@gmail.com>
2021-05-30 11:53:18 -0300

    Ajuste requisito 3. Ajuste apos verificaçao do teste.

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-    U.user_name AS usuario, S.song_name AS nome
+    S.song_name AS nome, U.user_name AS usuario
 FROM SpotifyClone.users AS U
 INNER JOIN SpotifyClone.songs_history AS SH ON U.user_id = SH.user_id
 INNER JOIN SpotifyClone.songs AS S ON SH.song_id = S.song_id
