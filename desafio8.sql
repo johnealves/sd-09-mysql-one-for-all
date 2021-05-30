a22a89fe05c7aae374b771cedba21b50fc90a5d4
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-05-30 12:54:03 -0300

    req 08

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON Usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.Seguindo WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.Historico WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
