4ee075e3083813f3ab004b58883f44cc29946aff
jhon <jhonatan.arguello@gmail.com>
2021-05-30 22:01:30 -0300

    Revisando o requisito 9 e corrigi a link do inner join que estava com a chave errada.

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,11 @@
-USE SpotifyClone;
 DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(200))
 BEGIN
 SELECT A.artist_name AS artista, AL.album_name AS album
 FROM SpotifyClone.artists AS A
-INNER JOIN SpotifyClone.albums AS AL ON A.artist_id = AL.album_id
-WHERE A.artist_name = artist_id
+INNER JOIN SpotifyClone.albums AS AL ON A.artist_id = AL.artist_id
+WHERE A.artist_name = artist
 ORDER BY album ASC;
 END $$
 
