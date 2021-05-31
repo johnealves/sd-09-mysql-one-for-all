e4f5578908b0b71856466ba03e55b584c0f2b037
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-31 15:11:09 -0300

    task 2 again

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
 CREATE VIEW estatisticas_musicais AS
-	SELECT COUNT(c.cancao) as `cancoes`, COUNT(a.artista) as `artistas`, COUNT(al.album) as `albuns` FROM SpotifyClone.cancao as c
-    INNER JOIN SpotifyClone.album as al ON al.album_id = c.album_id
-    INNER JOIN SpotifyClone.artista as a ON a.artista_id = al.artista_id;
+    SELECT (SELECT COUNT(cancao) FROM SpotifyClone.cancao) AS `cancoes`,
+        (SELECT COUNT(artista) FROM SpotifyClone.artista) AS `artistas`,
+        (SELECT COUNT(album) FROM SpotifyClone.album) AS `albuns`;
