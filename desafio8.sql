8a45b6cbaeae014905f5528cc1f2e5fd79f1e4d1
Carine Becker <caarine.m@gmail.com>
2021-05-31 19:31:18 -0300

    feat: desafio8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users FOR EACH ROW 
+    BEGIN 
+		DELETE FROM SpotifyClone.activity WHERE user_id = OLD.user_id;
+		DELETE FROM SpotifyClone.following_artists WHERE user_id = OLD.user_id;
+    END;$$
+DELIMITER ;
