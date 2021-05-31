ae798a73e3c60dc1158bb4c50b2d83b1cacb489e
jdaniel-ap <jdaniell.93@gmail.com>
2021-05-31 19:46:41 -0300

    test

--- desafio1.sql
+++ desafio1.sql
@@ -1,125 +1,139 @@
-DROP SCHEMA IF EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS SpotifyClone;
+
 CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
--- criação de tabelas Normalizadas
-CREATE TABLE Planos (
-	plano_id INT PRIMARY KEY AUTO_INCREMENT,
-    plano VARCHAR(20) NOT NULL,
-    valor_plano DECIMAL(5,2) NOT NULL
-);
+CREATE TABLE planos (
+    plano_id INT PRIMARY KEY AUTO_INCREMENT,
+    plano VARCHAR(30) NOT NULL,
+    valor_plano FLOAT NOT NULL
+)  ENGINE=INNODB;
 
-CREATE TABLE Usuarios (
-	usuario_id INT PRIMARY KEY AUTO_INCREMENT ,
-    usuario_nome VARCHAR(40) NOT NULL,
-    usuario_idade INT NOT NULL,
+CREATE TABLE artistas (
+    artista_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
+)  ENGINE=INNODB;
+
+CREATE TABLE albuns (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album VARCHAR(50) NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (artista_id)
+        REFERENCES artistas (artista_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE usuarios (
+    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
     plano_id INT NOT NULL,
-    FOREIGN KEY (plano_id) REFERENCES Planos(plano_id)
-);
+    FOREIGN KEY (plano_id)
+        REFERENCES planos (plano_id)
+)  ENGINE=INNODB;
 
-CREATE TABLE Artistas (
-	artista_id INT PRIMARY KEY AUTO_INCREMENT,
-    artista_nome VARCHAR(40) NOT NULL
-);
-
-CREATE TABLE Albums(
-	album_id INT PRIMARY KEY AUTO_INCREMENT,
-    artista_id INT NOT NULL,
-    album_nome VARCHAR(30) NOT NULL,
-    FOREIGN KEY (artista_id) REFERENCES Artistas(artista_id)
-);
-
-CREATE TABLE Cancoes(
-	cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE cancoes (
+    cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+    cancao VARCHAR(100) NOT NULL,
     album_id INT NOT NULL,
-    cancao_nome VARCHAR(40) NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
-);
+    FOREIGN KEY (album_id)
+        REFERENCES albuns (album_id)
+)  ENGINE=INNODB;
 
-CREATE TABLE Seguidores (
-    usuario_id INT NOT NULL,
-    artista_id INT NOT NULL,
-    PRIMARY KEY (usuario_id, artista_id),
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id),
-    FOREIGN KEY (artista_id) REFERENCES Artistas(artista_id)
-);
-
-CREATE TABLE Historicos (
+CREATE TABLE historico_reproducao (
     usuario_id INT NOT NULL,
     cancao_id INT NOT NULL,
-    PRIMARY KEY (usuario_id, cancao_id),
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id),
-    FOREIGN KEY (cancao_id) REFERENCES Cancoes(cancao_id)
-);
+    FOREIGN KEY (usuario_id)
+        REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (cancao_id)
+        REFERENCES cancoes (cancao_id),
+    PRIMARY KEY (usuario_id , cancao_id)
+)  ENGINE=INNODB;
 
--- Povoando as tabelas com os dados
-INSERT INTO Planos (plano, valor_plano)
-VALUES	('gratuito', 0),
-		('familiar', 7.99),
-        ('universitário', 5.99);
+CREATE TABLE seguindo_artistas (
+    usuario_id INT NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (usuario_id)
+        REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (artista_id)
+        REFERENCES artistas (artista_id),
+    PRIMARY KEY (usuario_id , artista_id)
+)  ENGINE=INNODB;
 
-INSERT INTO Usuarios (usuario_nome, usuario_idade, plano_id)
-VALUES	('Thati', 23, 1),
-		('Cintia', 35, 2),
-        ('Bill', 20, 3),
-        ('Roger', 45, 1);
+INSERT INTO planos (plano, valor_plano)
+VALUES
+  ('Gratuíto', 0.00),
+  ('Universitário', 5.99),
+  ('Familiar', 7.99);
 
-INSERT INTO Artistas (artista_nome)
-VALUES	('Walter Phoenix'),
-		('Peter Strong'),
-        ('Lance Day'),
-        ('Freedie Shannon');
+INSERT INTO artistas (nome)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
 
-INSERT INTO Albums (artista_id, album_nome)
-VALUES	(1, 'Envious'),
-		(1, 'Exuberant'),
-        (2, 'Hallowed Steam'),
-        (3, 'Incandescent'),
-        (4,'Temporary Culture');
-        
-INSERT INTO Cancoes (album_id, cancao_nome)
-VALUES	(1,'Soul For Us'),
-		(1,'Reflections Of Magic'),
-        (1,'Dance With Her Own'),
-        (2,'Troubles Of My Inner Fire'),
-        (2,'Time Fireworks'),
-        (3,'Magic Circus'),
-        (3,'Honey, So Do I'),
-        (3,'Sweetie, Let\'s Go Wild'),
-        (3,'She Knows'),
-        (4,'Fantasy For Me'),
-        (4,'Celebration Of More'),
-        (4,'Rock His Everything'),
-        (4,'Home Forever'),
-        (4,'Diamond Power'),
-        (4,'Honey, Let\'s Be Silly'),
-        (5,'Thang Of Thunder'),
-		(5,'Words Of Her Life'),
-        (5,'Without My Streets');
+INSERT INTO albuns (album, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
 
-INSERT INTO Seguidores (usuario_id, artista_id)
-VALUES	(1, 1),
-		(1, 3),
-        (1, 4),
-        (2, 1),
-        (2, 3),
-        (3, 1),
-        (3, 2),
-        (4, 4);
+INSERT INTO usuarios (nome, idade, plano_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 3),
+  ('Bill', 20, 2),
+  ('Roger', 45, 1);
 
-INSERT INTO Historicos(usuario_id, cancao_id)
-VALUES	(1, 1),
-		(1, 6),
-        (1, 14),
-        (1, 16),
-        (2, 13),
-        (2, 17),
-        (2, 2),
-        (2, 15),
-        (3, 4),
-        (3, 16),
-        (3, 6),
-        (4, 3),
-        (4, 18),
-		(4, 11);
+INSERT INTO cancoes (cancao, album_id)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+
+INSERT INTO historico_reproducao (usuario_id, cancao_id)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 4),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
+
+INSERT INTO seguindo_artistas (usuario_id, artista_id)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
+  
