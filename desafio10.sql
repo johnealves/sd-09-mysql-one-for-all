f6b4472cd98522f3751c4c52160963615cc74b00
antonio arieiro <sfgfffs@gmail.com>
2021-05-31 13:44:54 -0300

    não entendi muito bem essa do json mas ta bom

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,9 @@
 USE SpotifyClone;
-DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico (id INT)
-RETURNS INT READS SQL DATA
-BEGIN
-DECLARE playlist_history_count INT;
-SELECT 	
-    COUNT(*)
-FROM
-    playlist_history
-WHERE
-    user_id = id INTO playlist_history_count;
-RETURN playlist_history_count;
-END $$
-DELIMITER ;
+DROP FUNCTION quantidade_musicas_no_historico;
+DELIMITER $$ CREATE FUNCTION quantidade_musicas_no_historico(id_user INT) RETURNS INT READS SQL DATA BEGIN
+DECLARE total_songs INT;
+SELECT COUNT(*)
+FROM reproductions_history AS rh
+WHERE rh.user_id = id_user INTO total_songs;
+RETURN total_songs;
+END $$ DELIMITER;
