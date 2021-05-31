e7d0892349765122811432b5224819730978a5c9
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-31 00:41:40 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+	  MIN(plano_valor) AS faturamento_minimo,
+    MAX(plano_valor) AS faturamento_maximo,
+    ROUND(AVG(plano_valor), 2) AS faturamento_medio,
+    SUM(plano_valor) AS faturamento_total
+FROM
+	  usuario AS usu
+INNER JOIN plano ON plano.plano_id = usu.plano_id;
