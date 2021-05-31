01d5b553a7b4506ed3aded8f6b3db0c89afef189
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-31 16:25:45 -0300

    task 4 again

--- desafio3.sql
+++ desafio3.sql
@@ -3,4 +3,3 @@ CREATE VIEW historico_reproducao_usuarios AS
     INNER JOIN SpotifyClone.historico as h ON u.usuario_id = h.usuario_id
     INNER JOIN SpotifyClone.cancao as c ON c.cancao_id = h.cancao_id
     ORDER BY `usuario`, `nome`;
-    
--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,13 @@
 CREATE VIEW top_3_artistas AS
-	SELECT a.artista AS `artista`, COUNT(s.artista_id) as `seguidores` FROM SpotifyClone.seguindo as s
-    INNER JOIN SpotifyClone.artista as a ON a.artista_id = s.artista_id
-    GROUP BY `artista`
-    ORDER BY `seguidores` DESC, `artista`
-    LIMIT 3;
-
-Error Code: 1054. Unknown column 's.artista_id' in 'field list'
+    SELECT 
+        a.artista AS `artista`,
+        (SELECT 
+                COUNT(s.artista_id)
+            FROM
+                SpotifyClone.seguindo AS s
+            WHERE
+                s.artista_id = a.artista_id) AS `seguidores`
+    FROM
+        SpotifyClone.artista AS a
+    ORDER BY `seguidores` DESC , `artista`
+    LIMIT 3
