cba3fadee39b1a8d1408d657b3195a53ad37a39a
saulokirchmaier <saulokirchmaier@gmail.com>
2021-05-31 11:39:25 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista (IN artista VARCHAR(255))
+BEGIN
+SELECT artistas.nome AS artistas,
+albuns.nome AS album
+FROM SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.albuns AS albuns
+ON artistas.artista_id = albuns.artista_id
+WHERE artistas.nome = artista;
+END $$
+DELIMITER ;
