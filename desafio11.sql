db0ed9596bafe1044e9d6bb04daab90cfc6712df
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-31 20:58:58 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,9 @@
 CREATE VIEW cancoes_premium AS
 SELECT
-    can.cancao_nome AS `nome`,
-    COUNT(*) AS `reproducoes`
-FROM
-    SpotifyClone.historico_de_reproducoes AS hr
-    INNER JOIN SpotifyClone.usuario AS usu ON hr.usuario_id = usu.usuario_id
-    INNER JOIN SpotifyClone.plano AS pl ON usu.plano_id = pl.plano_id
-    INNER JOIN SpotifyClone.cancao AS can ON hr.cancao_id = can.cancao_id
-WHERE
-    pl.plano_tipo != `gratuito`
-GROUP BY
-    `nome`
-ORDER BY
-    `nome`;
+    can.cancao_nome AS nome,
+    COUNT(hr.cancao_id) AS reproducoes
+FROM SpotifyClone.historico_de_reproducoes AS hr
+	INNER JOIN SpotifyClone.cancao AS can ON can.cancao_id = hr.cancao_id
+	INNER JOIN SpotifyClone.usuario AS usu ON hr.usuario_id = usu.usuario_id AND usu.plano_id <> 1
+GROUP BY nome
+ORDER BY nome;
