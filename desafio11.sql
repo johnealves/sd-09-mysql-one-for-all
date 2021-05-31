ec1ea4a40d23c0ecf7157736febb505923bad197
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-05-31 18:40:25 -0300

    Fix desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,6 @@
 CREATE VIEW cancoes_premium AS
-SELECT s.song_name AS nome, COUNT(*) AS reproducoes FROM SpotifyClone.play_history AS ph
+SELECT s.song_name AS nome, COUNT(*) AS reproducoes
+FROM SpotifyClone.play_history AS ph
 INNER JOIN SpotifyClone.song AS s ON ph.song_id = s.song_id
 INNER JOIN SpotifyClone.user AS u ON u.user_id = ph.user_id
 INNER JOIN SpotifyClone.account AS a ON a.account_id = u.account_id
