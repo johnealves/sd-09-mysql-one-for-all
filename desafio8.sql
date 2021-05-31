36b9e848ad8781c7d068daefaaeecb08617d2a43
saulokirchmaier <saulokirchmaier@gmail.com>
2021-05-31 11:04:14 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_de_reproducoes WHERE historico_de_reproducoes.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas WHERE seguindo_artistas.usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
