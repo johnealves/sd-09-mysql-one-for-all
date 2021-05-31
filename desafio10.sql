d3cf1d48768002c831571ced50223f85135027c7
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-31 11:50:30 -0300

    desafio 10 cc

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,12 @@
 delimiter $$
-create function quantidade_musicas_no_historico(nome_usuario varchar(100))
+create function quantidade_musicas_no_historico(usuarioId tinyint)
 returns tinyint reads sql data
 begin
 declare quantidade_musicas tinyint;
 select count(hi.musica_id) as quantidade_musicas_no_historico
 from SpotifyClone.historico as hi
 inner join SpotifyClone.usuarios as us on us.usuario_id = hi.usuario_id
-where us.nome =  nome_usuario
+where us.usuario_id =  usuarioId
 into quantidade_musicas;
 return quantidade_musicas;
 end $$
