8fd19d11e1d1da6018b41aad7c619468b6b5f9a5
Daniela Muller <danielamuller20@gmail.com>
2021-06-01 20:16:49 -0300

    avaliador

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-    AFTER DELETE ON SpotifyClone.usuarios FOR EACH ROW
+    BEFORE DELETE ON SpotifyClone.usuarios FOR EACH ROW
 BEGIN
     DELETE FROM SpotifyClone.historico_reproducao
     WHERE OLD.usuario_id = usuario_id;
