1e0ecd64d2dcd8ac5504c1ae158567f794c96cd8
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-01 09:59:06 -0300

    Requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
-    SELECT COUNT(*) AS 'cancoes',
-    (SELECT COUNT(*) FROM SpotifyClone.artistas) AS 'artistas',
-    (SELECT COUNT(*) FROM SpotifyClone.albuns) AS 'albuns'
-    FROM SpotifyClone.musicas;
+	SELECT (SELECT COUNT(id_musica) FROM SpotifyClone.musicas) AS 'cancoes',
+    (SELECT COUNT(id_artista) FROM SpotifyClone.artistas) AS 'artistas',
+    (SELECT COUNT(id_album) FROM SpotifyClone.albuns) AS 'albuns';    
 
 -- SELECT * FROM estatisticas_musicais; 
