167097bfd684afa53b164374a3b1ab2996e72f51
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-01 19:00:16 -0300

    feat/desafio7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+CREATE VIEW perfil_artistas AS
+SELECT A.`artist_name` AS `artista`, AL.`album_name` AS `album`, COUNT(F.artist_id) AS `seguidores`
+FROM SpotifyClone.artists AS A
+INNER JOIN SpotifyClone.Albums AS AL ON AL.artist_id = A.artist_id
+INNER JOIN SpotifyClone.Following AS F ON F.artist_id = A.artist_id
+GROUP BY AL.`album_name`
+ORDER BY `seguidores` DESC, `artist_name` ASC, `album` ASC;
