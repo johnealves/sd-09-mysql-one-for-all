17852ea9db90daa48013b7d63a2583b3a99970ac
Christopher <chris.tryber@gmail.com>
2021-06-01 14:07:38 -0300

    corrigi req 9

--- desafio9.sql
+++ desafio9.sql
@@ -6,7 +6,7 @@ BEGIN
     SELECT artist_id FROM SpotifyClone.artists WHERE artist_name = name_artist
 	INTO id_artist;
 
-	SELECT ar.artist_name, al.album_name FROM SpotifyClone.artists AS ar
+	SELECT ar.artist_name AS 'artista', al.album_name AS 'album' FROM SpotifyClone.artists AS ar
 	INNER JOIN SpotifyClone.albums AS al
 	ON ar.artist_id = id_artist AND al.artist_id = id_artist;
 END $$ 
