7cff9442ee600ff5373cc755e1c20c44abdff87b
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-01 18:54:12 -0300

    fix/desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(S.song_id) AS `cancoes`,
-  (SELECT COUNT(A.artist_id) FROM SpotifyClone.artists AS A) AS `artistas`,
-  (SELECT COUNT(AL.album_id) FROM SpotifyClone.albums AS AL) AS `albuns`
-FROM SpotifyClone.songs AS S;
+  (SELECT COUNT(A.artist_id) FROM SpotifyClone.Artists AS A) AS `artistas`,
+  (SELECT COUNT(AL.album_id) FROM SpotifyClone.Albums AS AL) AS `albuns`
+FROM SpotifyClone.Songs AS S;
