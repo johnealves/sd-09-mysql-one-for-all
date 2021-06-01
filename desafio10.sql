a423d93fd763cde9c3897d7dde79829af7d4795f
brenno-calado <brenno.calado@gmail.com>
2021-06-01 10:48:06 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(user_name VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE amount INT;
+    SELECT COUNT(h.user_id) INTO amount
+    FROM SpotifyClone.music_history AS h
+    INNER JOIN SpotifyClone.`user` AS u ON u.user_id = h.user_id
+    WHERE u.user_name LIKE user_name;
+    RETURN amount;
+END $$
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico('Bill');
