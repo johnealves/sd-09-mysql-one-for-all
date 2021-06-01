64c50b3e14110a257c66b3caed73033844baffc2
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-01 20:56:43 -0300

    fix 1

--- desafio1.sql
+++ desafio1.sql
@@ -10,6 +10,14 @@ CREATE TABLE planos(
     valor_plano DOUBLE NOT NULL
 ) engine = InnoDB;
 
+CREATE TABLE usuarios(
+    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(50) NOT NULL,
+    idade INT NOT NULL,
+	plano_id INT NOT NULL,
+    FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
+) engine = InnoDB;
+
 CREATE TABLE artistas(
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
     artista VARCHAR(50) NOT NULL
@@ -18,21 +26,7 @@ CREATE TABLE artistas(
 CREATE TABLE albuns(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     album VARCHAR(50) NOT NULL,
-    artista_id DOUBLE NOT NULL,
-    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
-) engine = InnoDB;
-
-CREATE TABLE usuarios(
-    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-    usuario VARCHAR(50) NOT NULL,
-    idade INT NOT NULL
-) engine = InnoDB;
-
-CREATE TABLE seguindo_artistas(
-    usuario_id INT NOT NULL,
-    artista_id INT NOT NULL ,
-	CONSTRAINT PRIMARY KEY(usuario_id, cancao_id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    artista_id INT NOT NULL,
     FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
@@ -51,18 +45,33 @@ CREATE TABLE historico_de_reproducao(
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
 ) engine = InnoDB;
 
+CREATE TABLE seguindo_artistas(
+    usuario_id INT NOT NULL,
+    artista_id INT NOT NULL ,
+	CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
+) engine = InnoDB;
+
 INSERT INTO planos (plano, valor_plano)
 VALUES
 	('gratuito', 0),
 	('universitário', 5.99),
 	('familiar', 7.99);
-    
+
 INSERT INTO usuarios (usuario, idade, plano_id)
 VALUES
 	('Thati', 23, 1),
 	('Cintia', 35, 3),
 	('Bill', 20, 2),
 	('Roger', 45, 1);
+    
+INSERT INTO artistas (artista)
+VALUES
+	('Walter Phoenix'),
+	('Peter Strong'),
+	('Lance Day'),
+	('Freedie Shannon');
     
 INSERT INTO albuns (album, artista_id)
 VALUES
@@ -72,26 +81,26 @@ VALUES
 	('Incandescent', 3),
 	('Temporary Culture', 4);
     
-INSERT INTO cancoes (cancao, album_id, artista_id)
+INSERT INTO cancoes (cancao, album_id)
 VALUES
-	('Soul For Us', 1, 1),
-	('Reflections Of Magic', 1, 1),
-	('Dance With Her Own', 1, 1),
-	('Troubles Of My Inner Fire', 2, 1),
-	('Time Fireworks', 2, 1),
-	('Magic Circus', 3, 2),
-	('Honey, So Do I', 3, 2),
-	("Sweetie, Let's Go Wild", 3, 2),
-	('She Knows', 3, 2),
-	('Fantasy For Me', 4, 3),
-	('Celebration Of More', 4, 3),
-	('Rock His Everything', 4, 3),
-	('Home Forever', 4, 3),
-	('Diamond Power', 4, 3),
-	("Honey, Let's Be Silly", 4, 3),
-	('Thang Of Thunder', 5, 4),
-	('Words Of Her Life', 5, 4),
-	('Without My Streets', 5, 4);
+	('Soul For Us', 1),
+	('Reflections Of Magic', 1),
+	('Dance With Her Own', 1),
+	('Troubles Of My Inner Fire', 2),
+	('Time Fireworks', 2),
+	('Magic Circus', 3),
+	('Honey, So Do I', 3),
+	("Sweetie, Let's Go Wild", 3),
+	('She Knows', 3),
+	('Fantasy For Me', 4),
+	('Celebration Of More', 4),
+	('Rock His Everything', 4),
+	('Home Forever', 4),
+	('Diamond Power', 4),
+	("Honey, Let's Be Silly", 4),
+	('Thang Of Thunder', 5),
+	('Words Of Her Life', 5),
+	('Without My Streets', 5);
     
 INSERT INTO historico_de_reproducao(usuario_id, cancao_id)
 VALUES
@@ -109,13 +118,6 @@ VALUES
 	(4, 3),
 	(4, 18),
 	(4, 11);
-    
-INSERT INTO artistas (artista)
-VALUES
-	('Walter Phoenix'),
-	('Peter Strong'),
-	('Lance Day'),
-	('Freedie Shannon');
 
 INSERT INTO seguindo_artistas(usuario_id, artista_id)
 VALUES
