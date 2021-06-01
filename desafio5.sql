64ec852480cfcdad9a9cbfa42c70efc204b7b55b
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-01 18:59:22 -0300

    feat/desafio5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT S.`song_name` AS `cancoes`, COUNT(H.song_id) AS `reproducoes`
+FROM SpotifyClone.Songs AS S
+INNER JOIN SpotifyClone.History AS H ON H.song_id = S.song_id
+GROUP BY S.`song_name`
+ORDER BY `reproducoes` DESC, `cancoes` ASC
+LIMIT 2;
