0fe75a13d3c54687e7bfa1041bb5e1252c7b376c
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-01 10:22:25 -0300

    Requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,13 @@
 SELECT 
-    nome_usuario
+    nome_usuario AS usuario,
+    nome_musica AS nome
 FROM
-    SpotifyClone.usuarios;
+    SpotifyClone.historico AS h
+
+INNER JOIN
+SpotifyClone.usuarios AS u ON h.id_usuario = u.id_usuario
+
+INNER JOIN
+SpotifyClone.musicas AS m ON h.id_musica = m.id_musica
+
+ORDER BY u.nome_usuario, m.nome_musica;
