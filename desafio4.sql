e1cbfb9f48667b7f557fc6dd703c333290c67cdc
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-01 10:53:08 -0300

    Requisito 4 com view

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,17 @@
-SELECT 
-    nome_artista AS artista,
-    COUNT(id_usuario) AS seguidores
-FROM
-    SpotifyClone.seguindo AS s
+CREATE VIEW top_3_artistas AS
+	SELECT 
+		nome_artista AS artista,
+		COUNT(id_usuario) AS seguidores
+	FROM
+		SpotifyClone.seguindo AS s
+		
+	INNER JOIN
+	SpotifyClone.artistas AS a ON s.id_artista = a.id_artista
+
+	GROUP BY nome_artista
+
+	ORDER BY seguidores DESC, nome_artista
+
+	LIMIT 3;
     
-INNER JOIN
-SpotifyClone.artistas AS a ON s.id_artista = a.id_artista
-
-GROUP BY nome_artista
-
-ORDER BY COUNT(id_usuario) DESC, nome_artista
-
-LIMIT 3;    
+-- SELECT * FROM top_3_artistas;
