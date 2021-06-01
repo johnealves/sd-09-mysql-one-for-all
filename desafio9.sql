f9cabe2e2bc6d18824696f35f05af1b16cbd504b
brenno-calado <brenno.calado@gmail.com>
2021-06-01 10:32:03 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(100))
+BEGIN
+    SELECT art.artist_name AS 'artista', al.album_name AS 'album'
+    FROM SpotifyClone.artist AS art
+    INNER JOIN SpotifyClone.album AS al ON al.artist_id = art.artist_id
+    WHERE art.artist_name LIKE CONCAT('%',artist,'%')
+    ORDER BY `album`;
+END $$
+DELIMITER ;
