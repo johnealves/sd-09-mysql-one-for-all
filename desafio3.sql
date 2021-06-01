dcdd3613f3ec264561295633826e898fc35b4288
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-01 10:24:37 -0300

    Requisito 3 com view

--- desafio3.sql
+++ desafio3.sql
@@ -1,13 +1,16 @@
-SELECT 
-    nome_usuario AS usuario,
-    nome_musica AS nome
-FROM
-    SpotifyClone.historico AS h
+CREATE VIEW historico_reproducao_usuarios AS
+	SELECT 
+		nome_usuario AS usuario,
+		nome_musica AS nome
+	FROM
+		SpotifyClone.historico AS h
 
-INNER JOIN
-SpotifyClone.usuarios AS u ON h.id_usuario = u.id_usuario
+	INNER JOIN
+	SpotifyClone.usuarios AS u ON h.id_usuario = u.id_usuario
 
-INNER JOIN
-SpotifyClone.musicas AS m ON h.id_musica = m.id_musica
+	INNER JOIN
+	SpotifyClone.musicas AS m ON h.id_musica = m.id_musica
 
-ORDER BY u.nome_usuario, m.nome_musica;
+	ORDER BY u.nome_usuario, m.nome_musica;
+    
+-- SELECT * FROM historico_reproducao_usuarios;
