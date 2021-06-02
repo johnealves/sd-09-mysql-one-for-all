054f823571d455661c8dde6fc51697c0b66d1dcf
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-02 15:36:11 -0300

    feat/Desafio11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT S.`song_name` AS `nome`, COUNT(*) AS `reproducoes`
+FROM SpotifyClone.Songs AS S
+WHERE S.`song_id` IN(
+  SELECT `song_id`
+  FROM SpotifyClone.History AS H
+  WHERE H.user_id IN(
+    SELECT `user_id`
+    FROM SpotifyClone.Users
+    WHERE `plan_id` IN (2,3))
+  )
+GROUP BY S.`song_name`
+ORDER BY S.`song_name`;
