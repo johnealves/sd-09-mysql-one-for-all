d01184a8715a4adea8a34d5e06ecf68bb174e4a5
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-06-02 00:18:02 -0300

    fix(desafio8.sql): trigger syntax

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.history as h WHERE h.usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.follows as f WHERE f.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.`history` as h WHERE h.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.`follows` as f WHERE f.usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
