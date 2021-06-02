4314fae1aa16234763e026adda5d462dc1702dc8
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-02 17:41:52 -0300

    Add req 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+	A.artista AS 'artista',
+  AB.album AS 'album',
+  COUNT(SA.usuario_id) AS 'seguidores'
+FROM SpotifyClone.albuns AS AB
+INNER JOIN SpotifyClone.artistas AS A ON AB.artista_id = A.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas AS SA ON SA.artista_id = AB.artista_id
+GROUP BY A.artista_id, AB.album;
