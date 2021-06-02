a43ebc1f9d45241673ab8d1baaa78258d60c2eda
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-06-01 23:17:19 -0300

    feat(desafio7.sql): create view perfil_artistas

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        al.album AS album,
+        ar.nome,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.follows AS f
+            WHERE
+                f.artist_id = al.artist_id
+            GROUP BY f.artist_id) AS seguidores
+    FROM
+        SpotifyClone.albuns al
+            INNER JOIN
+        SpotifyClone.artists AS ar ON al.artist_id = ar.artist_id
+    ORDER BY seguidores DESC;
