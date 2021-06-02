2fb017c067ea45debb5659387557e759ff12e0f3
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-02 18:08:01 -0300

    Fix req 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.historico_de_reproducoes
-  WHERE historico_de_reproducoes.usuario_id = OLD.usuario_id;
+	DELETE FROM SpotifyClone.historico_de_reproducao
+  WHERE historico_de_reproducao.usuario_id = OLD.usuario_id;
   DELETE FROM SpotifyClone.seguindo_artista
   WHERE seguindo_artista.usuario_id = OLD.usuario_id;
 END $$
