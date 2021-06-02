8caec231f874d67e1f07e0c9a2aa5ec0b331abab
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-02 17:54:18 -0300

    Add req 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.historico_de_reproducoes
+  WHERE historico_de_reproducoes.usuario_id = OLD.usuarios_id;
+  DELETE FROM SpotifyClone.seguindo_artista
+  WHERE seguindo_artista.usuario_id = OLD.usuarios_id;
+END $$
+DELIMITER ;
