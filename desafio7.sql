31222a64a6b1cc7d5b46100228ed1b27b708d0b7
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-02 17:46:11 -0300

    fix req 7

--- desafio7.sql
+++ desafio7.sql
@@ -6,4 +6,5 @@ SELECT
 FROM SpotifyClone.albuns AS AB
 INNER JOIN SpotifyClone.artistas AS A ON AB.artista_id = A.artista_id
 INNER JOIN SpotifyClone.seguindo_artistas AS SA ON SA.artista_id = AB.artista_id
-GROUP BY A.artista_id, AB.album;
+GROUP BY A.artista_id, AB.album
+ORDER BY 3 DESC, 1, 2;
