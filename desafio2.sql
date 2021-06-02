b7aeb75c11f0e94382fa84d0cb64a63ec1065820
Arthur Nunes <coletivosuburbano@gmail.com>
2021-06-02 16:49:06 -0300

    Ex 2

---
+++ desafio2.sql
@@ -0,0 +1,17 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+  COUNT(*) AS 'cancoes',
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      `SpotifyClone`.`Artists`
+  ) AS 'artistas',
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      `SpotifyClone`.`Albums`
+  ) AS 'albuns'
+FROM
+  `SpotifyClone`.`Songs`;
