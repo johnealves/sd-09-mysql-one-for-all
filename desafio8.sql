da10326ef286d840e1487c0421065c23ff028fed
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-06-02 12:42:53 -0300

    requirement 8 correct attemp

--- desafio8.sql
+++ desafio8.sql
@@ -2,7 +2,7 @@ USE SpotifyClone;
 
 DELIMITER $$ 
 create trigger trigger_usuario_delete
-after delete on usuarios
+before delete on usuarios
 FOR each row
 BEGIN
 delete from SpotifyClone.seguindo where usuario_id = old.usuario_id;
