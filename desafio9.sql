305e1198775069b41acad59d3001407f257b69d2
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-03 01:22:52 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(45))
+BEGIN
+	SELECT 
+		nome_artista AS artista,
+		nome_album AS album
+	FROM
+		SpotifyClone.albuns AS alb
+		
+	INNER JOIN
+	SpotifyClone.artistas AS art ON alb.id_artista = art.id_artista
+
+	ORDER BY album;
+END $$
+
+DELIMITER ;
+
+-- CALL albuns_do_artista('Walter Phoenix');
