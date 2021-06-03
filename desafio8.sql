28e72d5145a6985a070a79d2e291888a70e43bb8
Micaela Alves <micaela.alves93@gmail.com>
2021-06-03 12:51:11 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+	AFTER DELETE ON usuario
+    FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.user_reproducoes WHERE id_usuario = old.id_usuario;
+	DELETE FROM SpotifyClone.user_seguindo WHERE id_usuario = old.id_usuario;
+END $$
+    DELIMITER ;
