3e10c53451c8760f737824aef446f03977eb8c7a
Maurice Alves <maurice.estevam@gmail.com>
2021-06-03 19:55:56 -0300

    Requisito - 8. Segunda tentativa.

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,14 @@
+USE SpotifyClone;
 DELIMITER $$
+
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
 DELETE FROM follow_stars
 WHERE users_id = OLD.users_id;
-DELETE FROM follow_stars AS s
-WHERE `history` = OLD.users_id;
+DELETE FROM `history`
+WHERE users_id = OLD.users_id;
 END
 $$
 DELIMITER ;
