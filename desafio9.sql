7915b9642262e3895f3b75f5be86814ba8be659b
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-03 18:16:05 -0300

    feat: complete challenge 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+USE spotifyclone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(
+IN artista VARCHAR(100))
+BEGIN
+	SELECT artista, albums.title AS album FROM albums
+    INNER JOIN artists ON albums.artist_id = artists.artist_id
+    WHERE artista = artists.name
+    ORDER BY albums.title ASC;
+END $$
+
+DELIMITER ;
+
+CALL albuns_do_artista("Walter Phoenix");
