6e0bbe7aee55f32a84e63e4c39a2265954b9cbcc
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-03 03:25:52 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,23 @@
+CREATE VIEW cancoes_premium AS
+	SELECT 
+		nome_musica AS nome,
+        COUNT(*) AS reproduçoes
+	FROM
+		SpotifyClone.historico AS h
+
+	INNER JOIN
+	SpotifyClone.musicas AS m ON h.id_musica = m.id_musica
+    
+    INNER JOIN
+	SpotifyClone.usuarios AS u ON h.id_usuario = u.id_usuario
+    
+    INNER JOIN
+	SpotifyClone.planos AS p ON u.id_usuario = p.id_plano
+    
+    WHERE p.id_plano = 2 OR p.id_plano = 3
+    
+    GROUP BY nome
+
+	ORDER BY nome;
+    
+-- SELECT * FROM cancoes_premium;
