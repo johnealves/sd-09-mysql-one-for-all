de6664e69441dd8037378d3ced982036f7e81fa7
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-03 17:53:46 -0300

    Requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,10 @@
-assasssa
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        c.cancao AS cancao, COUNT(c.cancao_id) AS reproducoes
+    FROM
+        SpotifyClone.cancoes AS c
+            INNER JOIN
+        SpotifyClone.historico_de_reproducao AS hr ON c.cancao_id = hr.cancao_id
+    GROUP BY c.cancao
+    ORDER BY reproducoes DESC , cancao ASC
+    LIMIT 2;
