e2461bb4b937f312d7129164fa8cd4f7a0daba9e
Layo Kaminski <layokaminski@gmail.com>
2021-06-03 12:56:31 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quantidade_musicas INT;
+SELECT 
+    COUNT(*)
+FROM
+	SpotifyClone.historical_reproduction
+WHERE
+    user_id = id INTO quantidade_musicas;
+    RETURN quantidade_musicas;
+END $$
+
+DELIMITER ;
