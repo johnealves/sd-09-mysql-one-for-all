d75721df867091e3a12cc5b08d3b955a06c1cc3a
Maurice Alves <maurice.estevam@gmail.com>
2021-06-04 11:17:52 -0300

    Requisito - 9.

---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(
+IN `name` VARCHAR(40))
+BEGIN
+SELECT
+CONCAT(stars.`name`, ' ', last_name) AS artista,
+album.album_name AS album
+FROM SpotifyClone.stars AS art
+INNER JOIN SpotifyClone.album AS alb
+ON art.stars_id = alb.stars_id
+WHERE art.`name` = `name`
+ORDER BY `album`;
+END $$
+
+DELIMITER ;
