24a6280166fd79645ccdd9a83ab8ba12c94ff99c
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-06-04 10:20:47 -0300

    Requisito 1

---
+++ desafio1.sql
@@ -0,0 +1,132 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE planos(
+    plano_id int NOT NULL AUTO_INCREMENT,
+    plano varchar(15) NOT NULL,
+    valor_plano decimal(15,2) NOT NULL,
+    PRIMARY KEY (plano_id)
+) engine = InnoDB;
+
+CREATE TABLE usuarios(
+    usuario_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    usuario varchar(40) NOT NULL,
+    idade int NOT NULL,
+    plano_id int,
+    FOREIGN KEY (plano_id) REFERENCES planos (plano_id)
+) engine = InnoDB;
+
+CREATE TABLE artistas(
+    artista_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    artista varchar(40) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE seguindoArtistas(
+    usuario_id int NOT NULL,
+    artista_id int NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas (artista_id),
+    CONSTRAINT PRIMARY KEY (usuario_id, artista_id)
+) engine = InnoDB;
+
+CREATE TABLE albuns(
+    album_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    album varchar(40) NOT NULL,
+    artista_id int NOT NULL,
+    FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
+) engine = InnoDB;
+
+CREATE TABLE musicas(
+    musica_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    musica varchar(40) NOT NULL,
+    album_id int NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albuns (album_id)
+) engine = InnoDB;
+
+CREATE TABLE usuarioMusicas(
+    usuario_id int NOT NULL,
+    musica_id int NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (musica_id) REFERENCES musicas (musica_id),
+    CONSTRAINT PRIMARY KEY (usuario_id, musica_id)
+) engine = InnoDB;
+
+INSERT INTO planos (plano, valor_plano)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO usuarios (usuario, idade, plano_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+  
+INSERT INTO artistas (artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+  
+INSERT INTO seguindoArtistas (usuario_id, artista_id)
+VALUES
+  (1,1),
+  (1,4),
+  (1,3),
+  (2,1),
+  (2,3),
+  (3,1),
+  (3,2),
+  (4,4);
+  
+INSERT INTO albuns (album, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+  
+INSERT INTO musicas (musica, album_id)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+  
+INSERT INTO usuarioMusicas (usuario_id, musica_id)
+VALUES
+  (1,1),
+  (1,6),
+  (1,14),
+  (1,16),
+  (2,13),
+  (2,17),
+  (2,2),
+  (2,15),
+  (3,4),
+  (3,16),
+  (3,6),
+  (4,3),
+  (4,18),
+  (4,11);
