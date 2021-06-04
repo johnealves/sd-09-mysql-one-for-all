d4744757dda98650b10796a929aa068d460e8a0b
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-06-04 06:40:53 -0300

    corriginto query desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -2,8 +2,8 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.user_name AS usuario,
 s.song AS nome
 FROM SpotifyClone.historic
-JOIN SpotifyClone.user_table AS U
-ON U.id = historic.user_id
-JOIN SpotifyClone.songs AS S
-ON S.id = historic.song_id
+JOIN SpotifyClone.user_table AS u
+ON u.id = historic.user_id
+JOIN SpotifyClone.songs AS s
+ON s.id = historic.song_id
 ORDER BY usuario ASC, nome ASC;
