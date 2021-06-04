627339a60a78723437ee06e55eb22a60199c3dbc
Maurice Alves <maurice.estevam@gmail.com>
2021-06-04 00:22:11 -0300

    Requisito - 8. Correção - 5.

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-
 DELIMITER $$
+
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
@@ -10,6 +10,3 @@ DELETE FROM `history`
 WHERE users_id = OLD.users_id;
 END $$
 DELIMITER ;
-
-DELETE FROM SpotifyClone.users
-WHERE user_name = "Thati";
