405b8de5535c608fe78a914ec3be80e263de9611
Maurice Alves <maurice.estevam@gmail.com>
2021-06-04 00:34:54 -0300

    Requisito - 8. Correção - 6.

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,11 @@
 DELIMITER $$
-
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
-DELETE FROM follow_stars
-WHERE users_id = OLD.users_id;
 DELETE FROM `history`
+WHERE users_id = OLD.users_id;
+DELETE FROM follow_stars
 WHERE users_id = OLD.users_id;
 END $$
 DELIMITER ;
