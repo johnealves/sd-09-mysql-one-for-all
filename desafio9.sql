42c90df8c7477d9cf87f36c69cf541d7ded6e36a
Leticia Lima <letylima96@gmail.com>
2021-06-04 13:45:43 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(35))
+BEGIN
+   SELECT art.artist_name AS 'artista', al.album_name AS 'album'
+    FROM SpotifyClone.artists_table AS art
+    JOIN SpotifyClone.albums_table AS al ON art.artist_id = al.artist_id
+    WHERE artist_name = artist_name;
+END $$
+
+DELIMITER ;
