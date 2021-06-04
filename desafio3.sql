67a47a15202c70134d473de72f6c832c7b736410
Leticia Lima <letylima96@gmail.com>
2021-06-04 13:38:43 -0300

    altera us para usr

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
     SELECT 
-        us.user_name AS 'usuario', sng.song_name AS 'nome'
+        usr.user_name AS 'usuario', sng.song_name AS 'nome'
     FROM
         SpotifyClone.history_table AS his
             JOIN
-        SpotifyClone.users_table AS us ON his.user_id = us.user_id
+        SpotifyClone.users_table AS usr ON his.user_id = usr.user_id
             JOIN
         SpotifyClone.songs_table AS sng ON his.song_id = sng.song_id
     ORDER BY `usuario` ASC , `nome` ASC;
