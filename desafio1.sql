07098c1defe5c1b98caca30f8b37abf2ac67fe70
Hudson Carlos <hc88806@gmail.com>
2021-06-05 00:34:28 -0400

    start project

---
+++ desafio1.sql
@@ -0,0 +1,124 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE PLANOS(
+    PLANO_ID INT PRIMARY KEY AUTO_INCREMENT,
+    TYPO VARCHAR(50) NOT NULL,
+    VALOR DOUBLE NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE USUARIOS(
+    USUARIO_ID INT PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR(50) NOT NULL,
+    IDADE INT NOT NULL,
+    PLANO_ID INT NOT NULL,
+    FOREIGN KEY (PLANO_ID) REFERENCES PLANOS(PLANO_ID)
+) engine = InnoDB;
+
+CREATE TABLE ARTISTAS(
+    ARTISTA_ID INT PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR(50) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE ALBUNS(
+    ALBUM_ID INT PRIMARY KEY AUTO_INCREMENT,
+    NOME_ALBUM VARCHAR(50) NOT NULL,
+    ARTISTA_ID INT NOT NULL,
+    FOREIGN KEY (ARTISTA_ID) REFERENCES ARTISTAS(ARTISTA_ID)
+) engine = InnoDB;
+
+CREATE TABLE MUSICAS(
+    MUSICA_ID INT PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR(50) NOT NULL,
+    ALBUM_ID INT NOT NULL,
+    FOREIGN KEY (ALBUM_ID) REFERENCES ALBUNS(ALBUM_ID)
+) engine = InnoDB;
+
+CREATE TABLE SEGUINDO_ARTISTA(
+    ARTISTA_ID INT NOT NULL,
+    USUARIO_ID INT NOT NULL,
+    FOREIGN KEY (ARTISTA_ID) REFERENCES ARTISTAS(ARTISTA_ID),
+    FOREIGN KEY (USUARIO_ID) REFERENCES USUARIOS(USUARIO_ID),
+    CONSTRAINT PRIMARY KEY(ARTISTA_ID, USUARIO_ID)
+) engine = InnoDB;
+
+CREATE TABLE HISTORICOS(
+    MUSICA_ID INT NOT NULL,
+    USUARIO_ID INT NOT NULL,
+    FOREIGN KEY (MUSICA_ID) REFERENCES MUSICAS(MUSICA_ID),
+    FOREIGN KEY (USUARIO_ID) REFERENCES USUARIOS(USUARIO_ID),
+    CONSTRAINT PRIMARY KEY(MUSICA_ID, USUARIO_ID)
+) engine = InnoDB;
+INSERT INTO PLANOS(TYPO, VALOR)
+VALUES
+  ('GRATUITO', 0),
+  ('FAMILIAR', 7.99),
+  ('UNIVERSITÁRIO', 5.99);
+INSERT INTO USUARIOS(NOME, IDADE, PLANO_ID)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+  INSERT INTO ARTISTAS(NOME)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+  INSERT INTO ALBUNS(NOME_ALBUM, ARTISTA_ID)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+  INSERT INTO MUSICAS(NOME, ALBUM_ID)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+  INSERT INTO SEGUINDO_ARTISTA(USUARIO_ID, ARTISTA_ID)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
+INSERT INTO HISTORICOS(USUARIO_ID, MUSICA_ID)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 4),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
