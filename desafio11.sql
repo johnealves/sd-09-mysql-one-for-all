21a83333105258aa48e92599fb6a7dd16b3c62a9
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-05 20:18:22 -0300

    created view with count join where atend params and set order

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+CREATE VIEW cancoes_premium AS
+SELECT s.song_name AS nome, COUNT(hist.user_id) AS reproducoes
+FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.history AS hist ON s.song_id = hist.song_id
+INNER JOIN SpotifyClone.users AS u ON hist.user_id = u.user_id
+WHERE u.plan_id IN (2 , 3)
+GROUP BY s.song_id ORDER BY 1 ASC;
