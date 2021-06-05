9803b998fab4c143825ecb3627d4724cce14e845
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-05 20:08:50 -0300

    solved bug on typo, sql reads camelCase, changed param name

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,12 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(user_ID int)
+CREATE FUNCTION quantidade_musicas_no_historico(u_ID int)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantity INT;
 SELECT
-COUNT(*) FROM SpotifyClone.history WHERE user_id = user_ID INTO quantity;
+COUNT(*) FROM SpotifyClone.history WHERE user_id = u_ID INTO quantity;
 RETURN quantity;
 END $$
 
