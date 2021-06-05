592d8562c4666c59bdc020889df7e680e060cbb8
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-05 18:52:49 -0300

    create view with params and set order with params

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT sg.song_name AS cancao, COUNT(hist.song_id) AS reproducoes
+FROM SpotifyClone.history AS hist
+INNER JOIN SpotifyClone.songs AS sg ON hist.song_id = sg.song_id
+GROUP BY sg.song_name ORDER BY 2 DESC , 1 ASC LIMIT 2;
