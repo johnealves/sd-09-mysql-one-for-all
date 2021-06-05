cb711db07459d46e3df1dd382956b5b141c3b9f9
Cainan <cainan6697@gmail.com>
2021-06-05 18:46:16 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        MIN(P.valor_plano) AS 'faturamento_minimo',
+        MAX(P.valor_plano) AS 'faturamento_maximo',
+        ROUND(AVG(P.valor_plano), 2) AS 'faturamento_medio',
+        SUM(P.valor_plano) AS 'faturamento_total'
+    FROM
+        usuarios AS U
+            INNER JOIN
+        planos AS P ON U.plano_id = P.plano_id;
