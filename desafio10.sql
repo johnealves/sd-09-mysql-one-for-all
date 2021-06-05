68290d7aaefd9348838e410fae3ae413d217a7ec
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-06-05 00:22:35 -0300

    Requisito 10 e 11

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total INT;
+SELECT COUNT(usuario_id) FROM usuarioMusicas
+WHERE usuario_id = user_id INTO total;
+RETURN total;
+END $$
+
+DELIMITER ;
