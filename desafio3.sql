a33cad7fcaf73383f973a7fac24b7dafabc05fa9
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-05 19:35:10 -0300

    solved typo bugs

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.`user` AS usuario, s.song_name AS nome
+SELECT u.user_name AS usuario, s.song_name AS nome
 FROM SpotifyClone.history AS hist
 INNER JOIN SpotifyClone.users AS u ON hist.user_id = u.user_id
 INNER JOIN SpotifyClone.songs AS s ON hist.song_id = s.song_id
--- desafio4.sql
+++ desafio4.sql
@@ -2,4 +2,4 @@ CREATE VIEW top_3_artistas AS
 SELECT art.artist_name AS artista, COUNT(flw.artist_id) AS seguidores
 FROM SpotifyClone.followin_artists AS flw
 INNER JOIN SpotifyClone.artists AS art ON flw.artist_id = art.artist_id
-GROUP BY art.artist ORDER BY 2 DESC , 1 ASC LIMIT 3;
+GROUP BY art.artist_name ORDER BY 2 DESC , 1 ASC LIMIT 3;
