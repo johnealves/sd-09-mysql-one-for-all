ed0df101d10e2bb07439c25407b00080b6c81a6e
LeonarDev <leonardo.majevski@gmail.com>
2021-06-06 18:16:08 -0300

    Requirement 8

---
+++ desafio8.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON 
+    SpotifyClone.USUARIOS
+  FOR EACH ROW
+BEGIN
+  DELETE FROM 
+    SpotifyClone.USUARIOS_REPRODUCOES UR
+  WHERE 
+    UR.usuario_id = OLD.usuario_id;
+  DELETE FROM
+    SpotifyClone.USUARIOS_SEGUIDAS US
+  WHERE 
+    US.usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
