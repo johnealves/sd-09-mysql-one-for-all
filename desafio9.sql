e2ed5a97cd72c57698d9cd7b9e5fd292b277e8c5
Maurice Alves <maurice.estevam@gmail.com>
2021-06-06 12:23:10 -0300

    Requisito - 9.

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,14 @@
-USE SpotifyClone;
+
 DELIMITER $$
 
-CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50))
+CREATE PROCEDURE albuns_do_artista(IN n_artista VARCHAR(50))
 BEGIN
-   SELECT a.`name` AS 'artista', al.album_name AS 'album'
-    FROM SpotifyClone.stars AS a
-    JOIN spotifyClone.album AS al ON a.stars_id = al.stars_id
-    WHERE `name` = artista;
+  SELECT CONCAT(a.`name`, ' ', a.last_name) AS 'artista', al.album_name AS 'album'
+   FROM SpotifyClone.stars AS a
+   INNER JOIN SpotifyClone.album AS al
+   ON a.stars_id = al.stars_id
+   WHERE a.`name` = n_artista
+   ORDER BY album, artista ASC;
 END $$
 
 DELIMITER ;
