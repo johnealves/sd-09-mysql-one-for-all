f8dacac967594d299258e4d3f2d31710a5a64293
LeonarDev <leonardo.majevski@gmail.com>
2021-06-06 17:52:54 -0300

    Fix Requirement 5

--- desafio5.sql
+++ desafio5.sql
@@ -8,6 +8,6 @@ CREATE VIEW top_2_hits_do_momento AS
     SpotifyClone.USUARIOS_REPRODUCOES UR ON C.cancao_id = UR.cancao_id
       INNER JOIN
     SpotifyClone.USUARIOS U ON U.usuario_id = UR.usuario_id
-  GROUP BY cancao
-  ORDER BY reproducoes DESC
+  GROUP BY UR.cancao_id
+  ORDER BY reproducoes DESC, cancao
   LIMIT 2;
