682ff3561c3506379ea67b3f4bbd74d25fbd7c39
Hudson Carlos <hc88806@gmail.com>
2021-06-07 11:34:18 -0400

    challenge 6 done

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,6 @@
 CREATE VIEW faturamento_atual AS
 SELECT CONVERT(MIN(VALOR), DECIMAL(3,2))  AS 'faturamento_minimo',
 MAX(VALOR) AS 'faturamento_maximo',
-CONVERT(SUM(VALOR) / 4, DECIMAL(3,2)) AS 'faturamento_medio',
+CONVERT(SUM(VALOR) / (
+SELECT count(*) FROM SpotifyClone.USUARIOS), DECIMAL(3,2)) AS 'faturamento_medio',
 SUM(VALOR) AS 'faturamento_total' FROM SpotifyClone.PLANOS;
