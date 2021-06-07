3c48cf5fb9b3a9041e47beb640334b979a30760b
Enio Nicael <enionicael@gmail.com>
2021-06-07 11:26:36 -0300

    Correcao de bug no desafio 6.

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,8 @@
-SELECT
+CREATE VIEW faturamento_atual AS
+	SELECT
 		MIN(p.valor) AS faturamento_minimo,
     MAX(p.valor) AS faturamento_maximo,
     ROUND(AVG(p.valor), 2) AS faturamento_medio,
     SUM(p.valor) AS faturamento_total
-FROM SpotifyClone.Planos AS p
-INNER JOIN SpotifyClone.Usuarios AS u ON u.plano_id = p.plano_id;
+	FROM SpotifyClone.Planos AS p
+  INNER JOIN SpotifyClone.Usuarios AS u ON u.plano_id = p.plano_id;
