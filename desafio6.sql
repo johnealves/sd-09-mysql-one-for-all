bc27dec89f8719330042a02fa0cbe6e617c2398c
Hudson Carlos <hc88806@gmail.com>
2021-06-07 11:47:51 -0400

    challenge 6 done

--- desafio6.sql
+++ desafio6.sql
@@ -3,4 +3,7 @@ SELECT CONVERT(MIN(VALOR), DECIMAL(3,2))  AS 'faturamento_minimo',
 MAX(VALOR) AS 'faturamento_maximo',
 CONVERT(SUM(VALOR) / (
 SELECT count(*) FROM SpotifyClone.USUARIOS), DECIMAL(3,2)) AS 'faturamento_medio',
-SUM(VALOR) AS 'faturamento_total' FROM SpotifyClone.PLANOS;
+(SELECT SUM(VALOR) FROM SpotifyClone.USUARIOS AS U
+INNER JOIN SpotifyClone.PLANOS AS P ON U.PLANO_ID = P.PLANO_ID) AS 'faturamento_total'
+FROM SpotifyClone.PLANOS;
+
