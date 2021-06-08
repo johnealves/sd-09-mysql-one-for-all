6d2af32ed7ee00ae246e61db953e600d433efe1e
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-06-08 11:07:29 -0300

    desafio 2 fix db include

--- desafio2.sql
+++ desafio2.sql
@@ -1,14 +1,13 @@
 CREATE VIEW `estatisticas_musicais` AS
-SELECT * FROM song;
 SELECT 
     COUNT(*) AS `cancoes`,
     (SELECT 
             COUNT(*)
         FROM
-            `artist`) AS `artistas`,
+            SpotifyClone.`artist`) AS `artistas`,
     (SELECT 
             COUNT(*)
         FROM
-            `album`) AS `albuns`
+            SpotifyClone.`album`) AS `albuns`
 FROM
     SpotifyClone.`song`;
