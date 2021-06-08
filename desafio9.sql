8e6b67434046162e56638803fce8abd15e717b2d
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-06-08 14:34:29 -0300

    desafio 9 ok

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE PROCEDURE `albuns_do_artista`(IN selected_artist VARCHAR(200))
+BEGIN
+	SELECT 
+	art.artist_name AS `artista`, alb.album_name AS `album`
+	FROM
+	SpotifyClone.`artist` AS art
+	INNER JOIN
+	SpotifyClone.`album` AS alb ON alb.artist_id = art.artist_id
+	WHERE
+	art.artist_name = selected_artist
+	ORDER BY `album`;
+END$$
+
+DELIMITER ;
