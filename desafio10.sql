3556a74cbbed625e1fff3600b654a993de726a08
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-06-08 15:47:54 -0300

    ajusting tasks 10

--- desafio10.sql
+++ desafio10.sql
@@ -3,9 +3,8 @@ CREATE FUNCTION `quantidade_musicas_no_historico`(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE `numero` INT;
-SELECT COUNT(*) FROM SpotifyClone.`historico` where user_id = id
+SELECT COUNT(*) FROM SpotifyClone.`historico` where usuario_id = id
 INTO `numero`;
 RETURN `numero`;
 END $$
 DELIMITER ;
-
