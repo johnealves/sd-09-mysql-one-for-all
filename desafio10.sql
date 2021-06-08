5d0aa84e62df6934c8606c4513a1f196eb9ebee6
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-06-08 15:26:41 -0300

    again

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION `quantidade_musicas_no_historico`(id INT)
 RETURN INT READS SQL DATA
 BEGIN
 DECLARE `numero` INT;
-SELECT COUTN(*) FROM SpotifyClone.historico where user_id = id
+SELECT COUTN(*) FROM SpotifyClone.`historico` where user_id = id
 INTO `numero`;
 RETURN `numero`;
 END $$
--- desafio8.sql
+++ desafio8.sql
@@ -2,11 +2,11 @@ USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER SpotifyClone `trigger_usuario_delete`
-  BEFORE DELETE ON SpotifyClone.usuario
+  BEFORE DELETE ON SpotifyClone.`usuario`
   FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.seguindo WHERE usuario_id = OLD.usuario_id;
-  DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.`seguindo` WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.`historico` WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
