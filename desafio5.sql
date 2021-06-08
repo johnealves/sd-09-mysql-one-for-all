e4dc9967518fb0892ca0ce9f8a6996d3f1317424
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-06-08 06:38:23 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT 
+songs.name AS 'cancao',
+COUNT(*) AS 'reproducoes'
+FROM
+SpotifyClone.songs AS songs
+INNER JOIN
+SpotifyClone.playback_history AS history ON history.song_id = songs.song_id
+GROUP BY songs.name
+ORDER BY 2 DESC, 1
+LIMIT 2
+;
