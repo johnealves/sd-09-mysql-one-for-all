fecef825872ab47b813e0467ea02e37bcace7ae0
ViniciusChagas <viniciushrcs@gmail.com>
2021-06-09 08:23:36 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+--USE SpotifyClone;
+
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(DISTINCT c.cancao_titulo) AS 'cancoes',
+COUNT(DISTINCT art.artista_nome) AS 'artistas',
+COUNT(DISTINCT alb.album_titulo) AS 'albuns'
+FROM  SpotifyClone.cancoes AS c, SpotifyClone.artistas AS art, SpotifyClone.albuns AS alb;
