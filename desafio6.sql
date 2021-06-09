35e6c0ea8e5311bb5aceb4e34fbcfe5787da369b
Hudson Carlos <hc88806@gmail.com>
2021-06-09 18:53:41 -0400

    fixing 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,5 @@ SELECT LTRIM(CONVERT(MIN(VALOR), DECIMAL(3,2))) AS 'faturamento_minimo',
 LTRIM(MAX(VALOR)) AS 'faturamento_maximo',
 LTRIM(CONVERT(SUM(VALOR) / (
 SELECT count(*) FROM SpotifyClone.USUARIOS), DECIMAL(3,2))) AS 'faturamento_medio',
-LTRIM(SUM(VALOR)) AS 'faturamento_total'
+FORMAT(SUM(VALOR), 2) AS 'faturamento_total'
 FROM SpotifyClone.PLANOS;
