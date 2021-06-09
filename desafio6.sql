a4167c0c0c04364f2e12a7273926733089f7665d
Hudson Carlos <hc88806@gmail.com>
2021-06-09 11:14:47 -0400

    challenge 7 done

--- desafio6.sql
+++ desafio6.sql
@@ -7,4 +7,3 @@ SELECT count(*) FROM SpotifyClone.USUARIOS), DECIMAL(3,2)) AS 'faturamento_medio
 (SELECT SUM(VALOR) FROM SpotifyClone.USUARIOS AS U
 INNER JOIN SpotifyClone.PLANOS AS P ON U.PLANO_ID = P.PLANO_ID) AS 'faturamento_total'
 FROM SpotifyClone.PLANOS;
-
