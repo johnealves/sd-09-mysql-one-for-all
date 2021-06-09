1dc03205584a71b160e66891a315aa55a12aadc6
Hudson Carlos <hc88806@gmail.com>
2021-06-09 18:05:46 -0400

    test chellenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,6 +3,6 @@ SELECT LTRIM(CONVERT(MIN(VALOR), DECIMAL(3,2))) AS 'faturamento_minimo',
 LTRIM(MAX(VALOR)) AS 'faturamento_maximo',
 LTRIM(CONVERT(SUM(VALOR) / (
 SELECT count(*) FROM SpotifyClone.USUARIOS), DECIMAL(3,2))) AS 'faturamento_medio',
-LTRIM((SELECT SUM(VALOR) FROM SpotifyClone.USUARIOS AS U
-INNER JOIN SpotifyClone.PLANOS AS P ON U.PLANO_ID = P.PLANO_ID)) AS 'faturamento_total'
+(SELECT SUM(VALOR) FROM SpotifyClone.USUARIOS AS U
+INNER JOIN SpotifyClone.PLANOS AS P ON U.PLANO_ID = P.PLANO_ID) AS 'faturamento_total'
 FROM SpotifyClone.PLANOS;
