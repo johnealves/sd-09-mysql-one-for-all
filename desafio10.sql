31e55d94b625b2be3c8c744ac46c232afffc6c45
Maurice Alves <maurice.estevam@gmail.com>
2021-06-12 17:14:01 -0300

    Requisitos - 8 e 10.

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,12 @@
-
 DELIMITER $$
 
 CREATE FUNCTION quantidade_musicas_no_historico(user_ID INT)
  RETURNS INT READS SQL DATA
 BEGIN
 DECLARE tot_musicas INT;
-SELECT COUNT(*)
-FROM SpotifyClone.`history`
-WHERE users_id = user_ID INTO tot_musicas;
+SELECT COUNT(users_id)
+FROM SpotifyClone.`history` AS h
+WHERE h.users_id = user_ID INTO tot_musicas;
 RETURN tot_musicas;
 END $$
 
--- desafio8.sql
+++ desafio8.sql
@@ -1,14 +1,22 @@
-
+use SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.`history`
-WHERE `history`.users_id = OLD.users_id;
 DELETE FROM SpotifyClone.follow_stars
-WHERE follow_stars.users_id = OLD.users_id;
+WHERE users_id = OLD.users_id; 
+DELETE FROM SpotifyClone.`history` 
+WHERE users_id = OLD.users_id;
 END $$
 
 DELIMITER ;
+
+-- use SpotifyClone;
+-- DELETE FROM users
+-- WHERE user_name = 'Thati';
+
+-- SET SQL_SAFE_UPDATES = 0;
+
+-- SELECT * from users;
