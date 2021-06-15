ee8e27e786ef83401b343d179bade8ff0b67bb0b
Kevin Fraga <kevinciolac@gmail.com>
2021-06-15 17:09:33 -0300

    desafio :two: fix :three:

--- desafio2.sql
+++ desafio2.sql
@@ -7,9 +7,13 @@ A segunda coluna deve exibir a quantidade total de artistas e deverá ter o alia
 A terceira coluna deve exibir a quantidade de álbuns e deverá ter o alias "albuns". */
 
 USE SpotifyClone;
--- DROP VIEW estatisticas_musicais;
+
 CREATE VIEW estatisticas_musicais AS
-  SELECT COUNT(*) AS cancoes, (SELECT COUNT(*) FROM artist) AS artistas, (SELECT COUNT(*) FROM album) AS albuns
-  FROM song;
+  SELECT COUNT(DISTINCT s.song) AS cancoes,
+    COUNT(DISTINCT art.artist) AS artistas,
+    COUNT(DISTINCT alb.album) AS albuns
+  FROM song s
+    INNER JOIN album alb ON s.album_id = alb.album_id
+    INNER JOIN artist art ON alb.artist_id = art.artist_id;
 
 SELECT * FROM estatisticas_musicais;
