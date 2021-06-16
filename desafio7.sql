308134164358085acb174a66059301e1c4010a97
Kevin Fraga <kevinciolac@gmail.com>
2021-06-16 01:27:23 -0300

    desafio :seven:

---
+++ desafio7.sql
@@ -0,0 +1,22 @@
+/* Mostre uma relação de todos os álbuns produzidos por cada pessoa artista,
+ com a quantidade de seguidores que ela possui, de acordo com os detalhes a seguir.
+ Para tal, crie uma VIEW chamada perfil_artistas, com as seguintes colunas:
+
+A primeira coluna deve exibir o nome da pessoa artista, com o alias "artista".
+
+A segunda coluna deve exibir o nome do álbum, com o alias "album".
+
+A terceira coluna deve exibir a quantidade de pessoas seguidoras que aquela pessoa artista possui e
+ deve possuir o alias "seguidores".
+
+Seus resultados devem estar ordenados de forma decrescente, baseando-se no número de pessoas seguidoras.
+ Em caso de empate no número de pessoas, ordene os resultados pelo nome da pessoa artista em ordem
+ alfabética e caso há artistas com o mesmo nome, ordene os resultados pelo nome do álbum alfabeticamente. */
+
+CREATE VIEW perfil_artistas AS
+  SELECT art.artist AS artista,
+    alb.album AS album,
+    (SELECT COUNT(*) FROM following WHERE artist_id = art.artist_id) AS seguidores
+  FROM album alb
+    INNER JOIN artist art ON alb.artist_id = art.artist_id
+  ORDER BY seguidores DESC, artista, album;
