8c59eb65e0bbc73290e612b770099750a23109cb
Bob <wend3llsouza@gmail.com>
2021-06-16 19:02:46 -0300

    :beetle: problemas com testes locais - Desafio 8 e 9

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuario
+FOR EACH ROW
+BEGIN
+  DELETE FROM seguindo WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM historico WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+USE SpotifyClone
+CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(20))
+BEGIN
+SELECT a.artista_nome AS artista,
+b.album_nome AS album
+FROM artista AS a 
+INNER JOIN
+album AS b ON b.artista_id = a.artista_id
+WHERE artista_nome = nome;
+END $$
+DELIMITER ;
