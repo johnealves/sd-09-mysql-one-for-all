a04b64114eceba60bbba1d7c0eb90779910a3da3
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-17 20:35:11 -0300

    Fix wrong case

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,7 @@
+USE SpotifyClone
+
 CREATE VIEW estatisticas_musicais AS
     SELECT
-        (SELECT COUNT(*) FROM spotifyclone.song) AS 'cancoes',
-        (SELECT COUNT(*) FROM spotifyclone.musician) AS 'artistas',
-        (SELECT COUNT(*) FROM spotifyclone.album) AS 'albuns';
+        (SELECT COUNT(*) FROM SpotifyClone.song) AS 'cancoes',
+        (SELECT COUNT(*) FROM SpotifyClone.musician) AS 'artistas',
+        (SELECT COUNT(*) FROM SpotifyClone.album) AS 'albuns';
