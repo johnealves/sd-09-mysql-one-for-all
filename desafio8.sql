4d1db5a712b32423fe07b07a5598e50f7b2153fd
Bob <wend3llsouza@gmail.com>
2021-06-17 09:08:21 -0300

    :beetle: test local não funcionando

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-  DELETE FROM seguindo WHERE usuario_id = OLD.usuario_id;
-  DELETE FROM historico WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.seguindo WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
