6f1857e753455283743df92dd3d84c335bb0bc15
Uibirá <usbarreto@gmail.com>
2021-06-18 00:23:33 -0300

    D03: comenta linha de consulta

--- desafio3.sql
+++ desafio3.sql
@@ -9,4 +9,4 @@ INNER JOIN
 SpotifyClone.cancoes AS c ON c.cancao_id = h.cancao_id
 ORDER BY u.usuario_nome, c.cancao_nome;
 
-select * from historico_reproducao_usuarios;
+-- select * from historico_reproducao_usuarios;
