3030abd37e6f6755da4e19f2dae8c3d0b93b0e79
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-18 12:07:11 -0300

    Challenge seven

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        musician_name AS artista,
+        album_name AS album,
+        COUNT(user_id) AS seguidores
+    FROM
+        SpotifyClone.album
+            JOIN
+        SpotifyClone.musician ON album.musician_id = musician.musician_id
+            JOIN
+        SpotifyClone.`following` ON `following`.musician_id = musician.musician_id
+    GROUP BY album
+    ORDER BY `seguidores` DESC , `artista` , `album`;
