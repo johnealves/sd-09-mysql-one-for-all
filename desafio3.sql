10fd39050c3423c981da875f35a6d188ccf74c67
Uibirá <usbarreto@gmail.com>
2021-06-18 00:17:03 -0300

    D03: corrige erros nas nomenclaturas

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-u.usuario_nome AS usuario, c.cancoes AS nome
+u.usuario_nome AS usuario, c.cancao_nome AS nome
 FROM
 SpotifyClone.usuarios AS u
 INNER JOIN
 SpotifyClone.historico AS h ON h.usuario_id = u.usuario_id
 INNER JOIN
 SpotifyClone.cancoes AS c ON c.cancao_id = h.cancao_id
-ORDER BY u.usuario_nome, s.cancao_nome;
+ORDER BY u.usuario_nome, c.cancao_nome;
+
+select * from historico_reproducao_usuarios;
