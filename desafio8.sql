3c287afe937f885eaf98c8ac73384c7a002e92dc
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-18 12:43:09 -0300

    Challenge eight

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.user
+FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.`following` WHERE user_id = OLD.user_id;
+  DELETE FROM SpotifyClone.playing_history WHERE user_id  = OLD.user_id;
+END //
+
+DELIMITER ;
