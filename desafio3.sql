5a4ca3ae1782a306e06bfe586438246bf76eafa5
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-18 09:31:03 -0300

    Challenge three

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+
+CREATE VIEW historico_reproducao_usuarios AS
+    SELECT 
+        user_name AS 'usuario', song_name AS 'nome'
+    FROM
+        SpotifyClone.playing_history
+            JOIN
+        SpotifyClone.song ON SpotifyClone.song.song_id = SpotifyClone.playing_history.song_id
+            JOIN
+        SpotifyClone.user ON SpotifyClone.user.user_id = SpotifyClone.playing_history.user_id
+    ORDER BY `usuario` , `nome`;
