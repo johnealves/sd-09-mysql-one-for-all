b6d59a459639701da5238043706abe13b86107f5
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-18 10:03:45 -0300

    Beautify challenge two

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,16 @@
 USE SpotifyClone;
 
 CREATE VIEW estatisticas_musicais AS
-    SELECT
-        (SELECT COUNT(*) FROM SpotifyClone.song) AS `cancoes`,
-        (SELECT COUNT(*) FROM SpotifyClone.musician) AS `artistas`,
-        (SELECT COUNT(*) FROM SpotifyClone.album) AS `albuns`;
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.song) AS `cancoes`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.musician) AS `artistas`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.album) AS `albuns`;
