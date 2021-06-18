823b8dede7328706a0daf2ba669a6c898c0e3a66
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-18 12:43:24 -0300

    Four spaces

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.user
 FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.`following` WHERE user_id = OLD.user_id;
-  DELETE FROM SpotifyClone.playing_history WHERE user_id  = OLD.user_id;
+    DELETE FROM SpotifyClone.`following` WHERE user_id = OLD.user_id;
+    DELETE FROM SpotifyClone.playing_history WHERE user_id  = OLD.user_id;
 END //
 
 DELIMITER ;
