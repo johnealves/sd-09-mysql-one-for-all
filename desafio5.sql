7ce67b42426d0b635fbb1ee68074a4155d54835c
Uibirá <usbarreto@gmail.com>
2021-06-19 16:51:00 -0300

    D05: corrige título da coluna

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,10 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT c.cancao_nome AS cancoes, COUNT(*) AS reproducoes
+SELECT c.cancao_nome AS cancao, COUNT(*) AS reproducoes
 FROM SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico AS h
 ON c.cancao_id = h.cancao_id
 GROUP BY c.cancao_nome
-ORDER BY reproducoes DESC, c.cancao_nome
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
 
 -- SELECT * from top_2_hits_do_momento;
