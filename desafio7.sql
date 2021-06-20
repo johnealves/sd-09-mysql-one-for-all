790be5c49409b1a9ab5236a0edd650620d35fb50
Uibirá <usbarreto@gmail.com>
2021-06-20 09:03:44 -0300

    D07

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+a.artista_nome AS "artista",
+b.album_nome AS "album",
+COUNT(*) AS "seguidores"
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
+INNER JOIN SpotifyClone.seguindo AS s ON a.artista_id = s.artista_id
+GROUP BY a.artista_nome, b.album_nome
+ORDER BY seguidores DESC, artista;
+
+-- A primeira coluna deve exibir o nome da pessoa artista, com o alias "artista".
+
+-- A segunda coluna deve exibir o nome do álbum, com o alias "album".
+
+-- A terceira coluna deve exibir a quantidade de pessoas seguidoras que aquela pessoa artista possui e deve possuir o alias "seguidores".
