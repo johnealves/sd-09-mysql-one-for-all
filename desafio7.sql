de12dba8fa16d1a922c6f358a624a13f93673b5b
Uibirá <usbarreto@gmail.com>
2021-06-20 12:34:03 -0300

    D07: outra alteração aleatória

--- desafio7.sql
+++ desafio7.sql
@@ -1,11 +1,11 @@
 CREATE VIEW perfil_artistas AS
 SELECT
-a.artista_nome AS "artista",
-b.album_nome AS "album",
-COUNT(*) AS "seguidores"
+a.artista_nome AS artista,
+b.album_nome AS album,
+COUNT(*) AS seguidores
 FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
-INNER JOIN SpotifyClone.seguindo AS s ON a.artista_id = s.artista_id
+INNER JOIN SpotifyClone.seguindo AS s ON b.artista_id = s.artista_id
 GROUP BY a.artista_nome, b.album_nome
 ORDER BY seguidores DESC, artista;
 
