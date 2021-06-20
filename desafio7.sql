f7d871b5a8ff54d481bb3c45550982b30d569353
Uibirá <usbarreto@gmail.com>
2021-06-20 13:15:03 -0300

    Outro foi

--- desafio7.sql
+++ desafio7.sql
@@ -8,12 +8,3 @@ INNER JOIN SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
 INNER JOIN SpotifyClone.seguindo AS s ON a.artista_id = s.artista_id
 GROUP BY a.artista_nome, b.album_nome
 ORDER BY seguidores DESC, artista;
-
--- SELECT * from perfil_artistas;
--- DROP VIEW top_2_hits_do_momento;
-
--- A primeira coluna deve exibir o nome da pessoa artista, com o alias "artista".
-
--- A segunda coluna deve exibir o nome do álbum, com o alias "album".
-
--- A terceira coluna deve exibir a quantidade de pessoas seguidoras que aquela pessoa artista possui e deve possuir o alias "seguidores".
