64023588b6b4c69fcaf4f98be6f19f61f45fb05a
Uibirá <usbarreto@gmail.com>
2021-06-20 12:47:01 -0300

    D07 e D08: alterações aleatórias

--- desafio7.sql
+++ desafio7.sql
@@ -5,7 +5,7 @@ b.album_nome AS album,
 COUNT(*) AS seguidores
 FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
-INNER JOIN SpotifyClone.seguindo AS s ON b.artista_id = s.artista_id
+INNER JOIN SpotifyClone.historico AS s ON a.artista_id = s.artista_id
 GROUP BY a.artista_nome, b.album_nome
 ORDER BY seguidores DESC, artista;
 
--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,13 @@
 USE SpotifyClone
+
 DELIMITER $$
+
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
 DELETE FROM historico WHERE usuario_id = OLD.usuario_id;
-END
-DELIMITER $$ ;
 
--- END $$
--- DELIMITER ;
+END $$
+
+DELIMITER ;
