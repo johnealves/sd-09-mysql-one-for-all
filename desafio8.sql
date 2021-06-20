e4c8593e05ab7f2ee5e214852aaa53d59659562c
Uibirá <usbarreto@gmail.com>
2021-06-20 13:22:23 -0300

    D8outra vez

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM seguindo WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
 
 END $$
 
