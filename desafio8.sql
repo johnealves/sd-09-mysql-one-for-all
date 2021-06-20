fb99f5de89b8a87529dcf84ebab83b8d957710b1
Uibirá <usbarreto@gmail.com>
2021-06-20 13:08:44 -0300

    Pequenas alterações nos D07 08 09. Boraaa

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico WHERE usuario_id = OLD.usuario_id;
+DELETE FROM seguindo WHERE usuario_id = OLD.usuario_id;
 
 END $$
 
--- desafio9.sql
+++ desafio9.sql
@@ -4,8 +4,9 @@ DELIMITER $$ -- definindo delimitador
 
 CREATE PROCEDURE albuns_do_artista (IN nomeArtista VARCHAR(50))
 BEGIN
-    SELECT a.artista_nome AS artista, b.album_nome AS album FROM artists AS a
-    INNER JOIN albuns AS b ON a.artista_id = b.artista_id
-    WHERE a.artista_nome = nomeArtista;
+SELECT a.artista_nome AS artista, b.album_nome AS album FROM artistas AS a
+INNER JOIN albuns AS b ON a.artista_id = b.artista_id
+WHERE a.artista_nome = nomeArtista;
+END $$
 
 DELIMITER ;
