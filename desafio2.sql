4c22b5e16adc6c962dd041295a53b5e8b6ae1f6b
Alstent <rafaelasltent@gmail.com>
2021-06-20 15:52:45 +0200

    desafio 2 e 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
 CREATE VIEW SpotifyClone.estatisticas_musicais AS
-  SELECT (SELECT COUNT(*) FROM SpotifyClone.cancoes),
-    (SELECT COUNT(*) FROM SpotifyClone.artistas),
-    (SELECT COUNT(*) FROM SpotifyClone.albums);
+  SELECT (SELECT COUNT(*) FROM SpotifyClone.cancoes) AS 'cancoes',
+    (SELECT COUNT(*) FROM SpotifyClone.artistas) AS 'artistas',
+    (SELECT COUNT(*) FROM SpotifyClone.albums) AS 'albuns';
---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+  SELECT u.nome AS 'usuario',
+    c.nome AS 'nome'
+  FROM SpotifyClone.historico_reproducoes AS hr
+    INNER JOIN SpotifyClone.usuarios AS u ON u.usuario_id = hr.usuario_id
+    INNER JOIN SpotifyClone.cancoes AS c ON c.cancao_id = hr.cancao_id
+  ORDER BY 1;
