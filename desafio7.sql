94aa89495d4e07876ab28767164530f6bb1a6d66
Uibirá <usbarreto@gmail.com>
2021-06-21 10:07:08 -0300

    D07: acrescenta album na ordenação

--- desafio7.sql
+++ desafio7.sql
@@ -7,7 +7,7 @@ FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
 INNER JOIN SpotifyClone.seguindo AS s ON a.artista_id = s.artista_id
 GROUP BY a.artista_nome, b.album_nome
-ORDER BY seguidores DESC, artista;
+ORDER BY seguidores DESC, artista, album;
 
--- SELECT * from perfil_artistas;
+SELECT * from perfil_artistas;
 -- DROP VIEW perfil_artistas;
