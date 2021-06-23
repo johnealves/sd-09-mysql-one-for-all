1788da37b46789bc37c51f2c8ecadfe02d7b3b4e
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-23 02:20:29 -0300

    consertando desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -4,4 +4,5 @@ COUNT(s.seguir_id) AS seguidores
 FROM SpotifyClone.seguindo_artistas AS s
 INNER JOIN SpotifyClone.artistas AS a ON s.artista_id = a.artista_id
 GROUP BY artista
-ORDER BY seguidores DESC, artista;
+ORDER BY seguidores DESC, artista
+LIMIT 3;
