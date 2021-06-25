a2c320cda72a968018999eeb99db705a4357ecf3
Cezene <cezenesn@hotmail.com.br>
2021-06-25 03:35:34 -0300

    .

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,6 @@
-/* */
+
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+SELECT
+( SELECT COUNT(*) FROM SpotifyClone.Cancao ) AS cancoes,
+( SELECT COUNT(*) FROM SpotifyClone.Artista ) AS artistas,
+( SELECT COUNT(*) FROM SpotifyClone.Album ) AS albuns;
