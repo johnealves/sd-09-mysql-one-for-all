f83b238b0beab471b7c16b637e21f8fcf2935ea1
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-25 13:44:05 -0300

    desafio5 - fix

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,5 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT  musica.musica AS `cancao`,
-        COUNT(musica.musica) AS `reproducoes`
-FROM SpotifyClone.musica
-JOIN SpotifyClone.historico_de_reproducao ON musica.musica_id = historico_de_reproducao.musica_id
-GROUP BY `cancao`
-ORDER BY  `reproducoes` DESC,
-          `cancao`
-LIMIT 2;
+SELECT t2.song AS cancao, COUNT(t1.song_id) AS reproducoes
+FROM SpotifyClone.history AS t1
+INNER JOIN SpotifyClone.songs AS t2 ON t1.song_id = t2.song_id
+GROUP BY t2.song ORDER BY 2 DESC , 1 ASC LIMIT 2;
