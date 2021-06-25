31660b651bcf1ebdd6230290a8925ed7d608d17e
victorvalim <victor.valim.c@gmail.com>
2021-06-25 02:48:59 -0300

    Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        artistas.artista AS artista,
+        COUNT(usuarios.usuario) AS seguidores
+    FROM
+        SpotifyClone.seguindo_artistas AS seguindo_artistas
+            INNER JOIN
+        SpotifyClone.artistas AS artistas ON seguindo_artistas.artista_id = artistas.artista_id
+            INNER JOIN
+        SpotifyClone.usuarios AS usuarios ON seguindo_artistas.usuario_id = usuarios.usuario_id
+    GROUP BY `artista`
+    ORDER BY `seguidores` DESC , `artista`
+    LIMIT 3;
