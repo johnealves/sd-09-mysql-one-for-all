4afd888782b3ec2a347eb72759f68b9358e4ff59
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-25 13:12:37 -0300

    restaurando ao ponto que passou

--- desafio1.sql
+++ desafio1.sql
@@ -1,148 +1,122 @@
-DROP DATABASE IF EXISTS `SpotifyClone`;
-CREATE DATABASE IF NOT EXISTS `SpotifyClone`;
-USE `SpotifyClone`;
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE SpotifyClone;
+USE SpotifyClone;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
-  `plano_id` TINYINT NOT NULL AUTO_INCREMENT,
-  `plano` VARCHAR(25) NOT NULL,
-  `valor` DECIMAL(4, 2) NOT NULL,
-  PRIMARY KEY(`plano_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE plans(
+  plan_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+  plan VARCHAR(10) NOT NULL,
+  price DOUBLE NOT NULL
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artista` (
-  `artista_id` INT NOT NULL AUTO_INCREMENT,
-  `artista` VARCHAR(25) NOT NULL,
-  PRIMARY KEY(`artista_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE users(
+  user_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+  `user` VARCHAR(20) NOT NULL,
+  age TINYINT NOT NULL,
+  plan_id TINYINT NOT NULL,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`album` (
-  `album_id` INT NOT NULL AUTO_INCREMENT,
-  `album` VARCHAR(50) NOT NULL,
-  `artista_id` INT NOT NULL,
-  PRIMARY KEY(`album_id`),
-  CONSTRAINT `fk_album_artista`
-    FOREIGN KEY(`artista_id`)
-    REFERENCES `SpotifyClone`.`artista` (`artista_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE artists(
+  artist_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+  artist VARCHAR(20) NOT NULL
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`musica` (
-  `musica_id` INT NOT NULL AUTO_INCREMENT,
-  `musica` VARCHAR(50) NOT NULL,
-  `album_id` INT NOT NULL,
-  PRIMARY KEY(`musica_id`),
-  CONSTRAINT `fk_musica_album`
-    FOREIGN KEY(`album_id`)
-    REFERENCES `SpotifyClone`.`album` (`album_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE albums(
+  album_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+  album VARCHAR(20) NOT NULL,
+  artist_id TINYINT NOT NULL,
+  FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario` (
-  `usuario_id` INT NOT NULL AUTO_INCREMENT,
-  `usuario` VARCHAR(25) NOT NULL,
-  `idade` TINYINT NOT NULL,
-  `plano_id` TINYINT NOT NULL,
-  PRIMARY KEY(`usuario_id`),
-  CONSTRAINT `fk_usuario_plano`
-    FOREIGN KEY(`plano_id`)
-    REFERENCES `SpotifyClone`.`plano` (`plano_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE songs(
+  song_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+  song VARCHAR(30) NOT NULL,
+  album_id TINYINT NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES albums(album_id)
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`historico_de_reproducao` (
-  `usuario_id` INT NOT NULL,
-  `musica_id` INT NOT NULL,
-  PRIMARY KEY(`usuario_id`, `musica_id`),
-  CONSTRAINT `fk_historico_usuario`
-    FOREIGN KEY(`usuario_id`)
-    REFERENCES `SpotifyClone`.`usuario` (`usuario_id`),
-  CONSTRAINT `fk_historico_musica`
-    FOREIGN KEY(`musica_id`)
-    REFERENCES `SpotifyClone`.`musica` (`musica_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `history`(
+  user_id TINYINT NOT NULL,
+  song_id TINYINT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (song_id) REFERENCES songs(song_id),
+  CONSTRAINT PRIMARY KEY(user_id, song_id)
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artista_seguido` (
-  `usuario_id` INT NOT NULL,
-  `artista_id` INT NOT NULL,
-  PRIMARY KEY(`usuario_id`, `artista_id`),
-  CONSTRAINT `fk_artista_seguido_usuario`
-    FOREIGN KEY(`usuario_id`)
-    REFERENCES `SpotifyClone`.`usuario` (`usuario_id`),
-  CONSTRAINT `fk_artista_seguido_artista`
-    FOREIGN KEY(`artista_id`)
-    REFERENCES `SpotifyClone`.`artista` (`artista_id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE followers(
+  user_id TINYINT NOT NULL,
+  artist_id TINYINT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+  CONSTRAINT PRIMARY KEY(user_id, artist_id)
+) engine = InnoDB;
 
-INSERT INTO `plano` (`plano`, `valor`) VALUES ('gratuito', 0);
-INSERT INTO `plano` (`plano`, `valor`) VALUES ('familiar', 7.99);
-INSERT INTO `plano` (`plano`, `valor`) VALUES ('universitário', 5.99);
+INSERT INTO plans (plan, price) VALUES
+('free', 0.00),
+('family', 7.99),
+('student', 5.99);
 
-INSERT INTO `artista` (`artista`) VALUES ('Walter Phoenix');
-INSERT INTO `artista` (`artista`) VALUES ('Peter Strong');
-INSERT INTO `artista` (`artista`) VALUES ('Lance Day');
-INSERT INTO `artista` (`artista`) VALUES ('Freedie Shannon');
+INSERT INTO users (`user`, age, plan_id) VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
 
-INSERT INTO `album` (`album`, `artista_id`) VALUES ('Envious', 1);
-INSERT INTO `album` (`album`, `artista_id`) VALUES ('Exuberant', 1);
-INSERT INTO `album` (`album`, `artista_id`) VALUES ('Hallowed Steam', 2);
-INSERT INTO `album` (`album`, `artista_id`) VALUES ('Incandescent', 3);
-INSERT INTO `album` (`album`, `artista_id`) VALUES ('Temporary Culture', 4);
+INSERT INTO artists (artist) VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Soul For Us', 1);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Reflections Of Magic', 1);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Dance With Her Own', 1);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Troubles Of My Inner Fire', 2);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Time Fireworks', 2);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Magic Circus', 3);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Honey, So Do I', 3);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Sweetie, Let\'s Go Wild', 3);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('She Knows', 3);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Fantasy For Me', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Celebration Of More', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Rock His Everything', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Home Forever', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Diamond Power', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Honey, Let\'s Be Silly', 4);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Thang Of Thunder', 5);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Words Of Her Life', 5);
-INSERT INTO `musica` (`musica`, `album_id`) VALUES ('Without My Streets', 5);
+INSERT INTO albums (album, artist_id) VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
-INSERT INTO `usuario` (`usuario`, `idade`, `plano_id`) VALUES ('Thati', 23, 1);
-INSERT INTO `usuario` (`usuario`, `idade`, `plano_id`) VALUES ('Cintia', 35, 2);
-INSERT INTO `usuario` (`usuario`, `idade`, `plano_id`) VALUES ('Bill', 20, 3);
-INSERT INTO `usuario` (`usuario`, `idade`, `plano_id`) VALUES ('Roger', 45, 1);
+INSERT INTO songs (song, album_id) VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Let\'s Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Let\'s Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (1, 1);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (1, 6);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (1, 14);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (1, 16);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (2, 13);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (2, 17);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (2, 2);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (2, 15);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (3, 4);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (3, 16);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (3, 6);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (4, 3);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (4, 18);
-INSERT INTO `historico_de_reproducao` (`usuario_id`, `musica_id`) VALUES (4, 11);
+INSERT INTO `history` (user_id, song_id) VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
 
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (1, 1);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (1, 4);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (1, 3);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (2, 1);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (2, 3);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (3, 2);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (3, 1);
-INSERT INTO `artista_seguido` (`usuario_id`, `artista_id`) VALUES (4, 4);
+INSERT INTO followers (user_id, artist_id) VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
